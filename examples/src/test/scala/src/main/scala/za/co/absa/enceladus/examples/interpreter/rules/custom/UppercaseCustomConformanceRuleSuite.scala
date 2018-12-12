9e783c021ce307cd7379c8440393060c0d9653de
David Benedeki <benedeki@volny.cz>
2018-12-12 11:12:50 +0100

    Feature #28: UTs for UppercaseCustomRuleInterpreter

---
+++ examples/src/test/scala/src/main/scala/za/co/absa/enceladus/examples/interpreter/rules/custom/UppercaseCustomConformanceRuleSuite.scala
@@ -0,0 +1,111 @@
+package src.main.scala.za.co.absa.enceladus.examples.interpreter.rules.custom
+
+import org.apache.spark.sql
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.scalatest.FunSuite
+import za.co.absa.enceladus.conformance.CmdConfig
+import za.co.absa.enceladus.conformance.interpreter.DynamicInterpreter
+import za.co.absa.enceladus.dao.{EnceladusDAO, EnceladusRestDAO}
+import za.co.absa.enceladus.model.Dataset
+import za.co.absa.enceladus.utils.testUtils.SparkTestBase
+
+
+case class TestInputRow(id: Int, mandatoryString: String, nullableString: Option[String])
+case class TestOutputRow(id: Int, mandatoryString: String, nullableString: Option[String], doneUpper: String)
+object TestOutputRow {
+  def apply(input: TestInputRow, doneUpper: String): TestOutputRow = TestOutputRow(input.id, input.mandatoryString, input.nullableString, doneUpper)
+}
+
+class UppercaseCustomConformanceRuleSuite extends FunSuite with SparkTestBase {
+
+
+  import spark.implicits._
+
+  implicit val progArgs: CmdConfig = CmdConfig() // here we may need to specify some parameters (for certain rules)
+  implicit val dao: EnceladusDAO = EnceladusRestDAO // you may have to hard-code your own implementation here (if not working with menas)
+  implicit val enableCF: Boolean = false
+
+  test("Golden flow") {
+    val input: Seq[TestInputRow] = Seq(
+      TestInputRow(1, "Hello world", Some("What a beautiful place")),
+      TestInputRow(4, "One Ring to rule them all", Some("One Ring to find them")),
+      TestInputRow(9, "ALREADY CAPS", None)
+    )
+    val inputData: DataFrame = spark.createDataFrame(input)
+    val conformanceDef: Dataset =  Dataset(
+      name = "Test golden flow", // whatever here
+      version = 0, //whatever here
+      hdfsPath = "/a/b/c",
+      hdfsPublishPath = "/publish/a/b/c",
+
+      schemaName = "Not really used here",
+      schemaVersion = 9999, //also not used
+
+      conformance = List(
+        UppercaseCustomConformanceRule(order = 0, outputColumn = "doneUpper", controlCheckpoint = false, inputColumn = "mandatoryString")
+      )
+    )
+
+    val outputData: sql.DataFrame = DynamicInterpreter.interpret(conformanceDef, inputData)
+    val output: Seq[TestOutputRow] = outputData.as[TestOutputRow].collect().toSeq
+    val expected: Seq[TestOutputRow] = (input zip Seq("HELLO WORLD", "ONE RING TO RULE THEM ALL", "ALREADY CAPS")).map(x => TestOutputRow(x._1, x._2))
+
+    assert(output === expected)
+  }
+
+  test("Integer value") {
+    val input: Seq[TestInputRow] = Seq(
+      TestInputRow(1, "Hello world", Some("What a beautiful place")),
+      TestInputRow(4, "One Ring to rule them all", Some("One Ring to find them")),
+      TestInputRow(9, "ALREADY CAPS", None)
+    )
+    val inputData: DataFrame = spark.createDataFrame(input)
+    val conformanceDef: Dataset =  Dataset(
+      name = "Test integer value", // whatever here
+      version = 0, //whatever here
+      hdfsPath = "/a/b/c",
+      hdfsPublishPath = "/publish/a/b/c",
+
+      schemaName = "Not really used here",
+      schemaVersion = 9999, //also not used
+
+      conformance = List(
+        UppercaseCustomConformanceRule(order = 0, outputColumn = "doneUpper", controlCheckpoint = false, inputColumn = "id")
+      )
+    )
+
+    val outputData: sql.DataFrame = DynamicInterpreter.interpret(conformanceDef, inputData)
+    val output: Seq[TestOutputRow] = outputData.as[TestOutputRow].collect().toSeq
+    val expected: Seq[TestOutputRow] = (input zip Seq("1", "4", "9")).map(x => TestOutputRow(x._1, x._2))
+
+    assert(output === expected)
+  }
+
+  test("Null value") {
+    val input: Seq[TestInputRow] = Seq(
+      TestInputRow(1, "Hello world", Some("What a beautiful place")),
+      TestInputRow(4, "One Ring to rule them all", Some("One Ring to find them")),
+      TestInputRow(9, "ALREADY CAPS", None)
+    )
+    val inputData: DataFrame = spark.createDataFrame(input)
+    val conformanceDef: Dataset =  Dataset(
+      name = "Test Null value", // whatever here
+      version = 0, //whatever here
+      hdfsPath = "/a/b/c",
+      hdfsPublishPath = "/publish/a/b/c",
+
+      schemaName = "Not really used here",
+      schemaVersion = 9999, //also not used
+
+      conformance = List(
+        UppercaseCustomConformanceRule(order = 0, outputColumn = "doneUpper", controlCheckpoint = false, inputColumn = "nullableString")
+      )
+    )
+
+    val outputData: sql.DataFrame = DynamicInterpreter.interpret(conformanceDef, inputData)
+    val output: List[TestOutputRow] = outputData.as[TestOutputRow].collect().toList
+    val expected: List[TestOutputRow] = (input zip Seq("WHAT A BEAUTIFUL PLACE", "ONE RING TO FIND THEM", null)).map(x => TestOutputRow(x._1, x._2)).toList
+
+    assert(output === expected)
+  }
+}
