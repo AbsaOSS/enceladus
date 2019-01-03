9ff445d96bb8694b3a4ebe43aec730e85847e1f8
Sasa Zejnilovic <zejnils@gmail.com>
2019-01-03 12:57:28 +0100

    Refactor of the data reader

---
+++ testutils/src/main/scala/za/co/absa/enceladus/testutils/DataframeReader.scala
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.testutils
+
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.{DataFrame, DataFrameReader, SparkSession}
+
+import scala.annotation.switch
+
+class DataframeReader(datasetPath: String, inputSchema: StructType = null)(implicit cmd: CmdConfig, sparkSession: SparkSession) {
+  lazy val dataFrame: DataFrame = getDataFrameReader.load(datasetPath)
+  lazy val dataFrameSchema: StructType = dataFrame.schema
+
+  def removeMetadataFromSchema: StructType = {
+    StructType(dataFrameSchema.map{ f => StructField(f.name, f.dataType, f.nullable) })
+  }
+
+  private def getDataFrameReader(implicit cmd: CmdConfig, sparkSession: SparkSession): DataFrameReader = {
+    (cmd.rawFormat: @switch) match {
+      case "csv" => getCsvReader
+      case "xml" => getXmlReader
+      case "fixed-width" => getFixedWidthReader
+      case "parquet" => getParquetReader
+      case _ => getStandardReader
+    }
+  }
+
+  private def getStandardReader()(implicit cmd: CmdConfig, sparkSession: SparkSession): DataFrameReader = {
+    sparkSession.read.format(cmd.rawFormat)
+  }
+
+  private def getParquetReader()(implicit cmd: CmdConfig, sparkSession: SparkSession): DataFrameReader = {
+    val dfReader = getStandardReader
+
+    if (inputSchema != null) dfReader.schema(inputSchema)
+    else dfReader
+  }
+
+  private def getFixedWidthReader()(implicit cmd: CmdConfig, sparkSession: SparkSession): DataFrameReader = {
+    val dfReader = getStandardReader
+
+    if (cmd.fixedWidthTrimValues.get) dfReader.option("trimValues", "true")
+    else dfReader
+  }
+
+  private def getXmlReader()(implicit cmd: CmdConfig, sparkSession: SparkSession): DataFrameReader = {
+    getStandardReader.option("rowTag", cmd.rowTag.get)
+  }
+
+  private def getCsvReader()(implicit cmd: CmdConfig, sparkSession: SparkSession): DataFrameReader = {
+    val dfReader = getStandardReader.option("delimiter", cmd.csvDelimiter.get)
+
+    if (cmd.csvHeader.isDefined) dfReader.option("header", cmd.csvHeader.get)
+    else dfReader
+  }
+}
---
+++ testutils/src/test/scala/za/co/absa/enceladus/testutils/ComparisonJobTest.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.testutils
+
+import java.nio.file.{Files, Paths}
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import za.co.absa.enceladus.utils.testUtils.SparkTestBase
+
+class ComparisonJobTest extends FunSuite with SparkTestBase with BeforeAndAfterAll {
+
+  val format = new SimpleDateFormat("yyyy_MM_dd-HH_mm")
+  var timePrefix = ""
+
+  override def beforeAll(): Unit = {
+    timePrefix = format.format(Calendar.getInstance().getTime())
+  }
+
+  test("Compare the same") {
+    val outPath = s"target/test_output/comparison_job/positive/$timePrefix"
+    import spark.implicits._
+
+    val args = Array(
+      "--raw-format", "csv",
+      "--delimiter", ",",
+      "--std-path", "src/test/resources/dataSample1.csv",
+      "--ref-path", "src/test/resources/dataSample2.csv",
+      "--out-path", outPath
+    )
+    ComparisonJob.main(args)
+
+    assert(!Files.exists(Paths.get(outPath)))
+  }
+
+  test("Compare the different") {
+    val refPath = "src/test/resources/dataSample1.csv"
+    val stdPath = "src/test/resources/dataSample3.csv"
+    val outPath = s"target/test_output/comparison_job/negative/$timePrefix"
+    val message = "Expected and actual datasets differ.\n" +
+                  s"Reference path: $refPath\n" +
+                  s"Actual dataset path: $stdPath\n" +
+                  s"Difference written to: $outPath\n" +
+                  "Count Expected( 10 ) vs Actual( 11 )"
+    import spark.implicits._
+
+    val args = Array(
+      "--raw-format", "csv",
+      "--delimiter", ",",
+      "--std-path", stdPath,
+      "--ref-path", refPath,
+      "--out-path", outPath
+    )
+
+    val caught = intercept[CmpJobDatasetsDiffer] {
+      ComparisonJob.main(args)
+    }
+
+    assert(caught.getMessage == message)
+    assert(Files.exists(Paths.get(outPath)))
+  }
+
+  test("Compare with wrong schema") {
+    val refPath = "src/test/resources/dataSample4.csv"
+    val stdPath = "src/test/resources/dataSample1.csv"
+    val outPath = s"target/test_output/comparison_job/negative/$timePrefix"
+    val diff = "List(StructField(_c5,StringType,true))"
+    val message = "Expected and actual datasets differ in schemas.\n" +
+                  s"Reference path: $refPath\n" +
+                  s"Actual dataset path: $stdPath\n" +
+                  s"Difference is $diff"
+
+    import spark.implicits._
+
+    val args = Array(
+      "--raw-format", "csv",
+      "--delimiter", ",",
+      "--std-path", stdPath,
+      "--ref-path", refPath,
+      "--out-path", outPath
+    )
+
+    val caught = intercept[CmpJobSchemasDiffer] {
+      ComparisonJob.main(args)
+    }
+
+    assert(caught.getMessage == message)
+  }
+}
