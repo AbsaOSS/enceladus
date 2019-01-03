8e7ffd96c733d1740bc5fb6be7a4b76e074a7321
Sasa Zejnilovic <zejnils@gmail.com>
2019-01-03 12:57:28 +0100

    Refactor of the data reader

--- testutils/src/main/scala/za/co/absa/enceladus/testutils/ComparisonJob.scala
+++ testutils/src/main/scala/za/co/absa/enceladus/testutils/ComparisonJob.scala
@@ -19,47 +19,51 @@ import org.apache.spark.SparkContext
 import org.apache.spark.sql.types.{StructField, StructType}
 import org.apache.spark.sql.{DataFrameReader, Dataset, Row, SparkSession}
 
+import scala.annotation.switch
+
+final case class CmpJobDatasetsDiffer(private val refPath: String,
+                                      private val stdPath: String,
+                                      private val outPath: String,
+                                      private val expectedCount: Long,
+                                      private val actualCount: Long,
+                                      private val cause: Throwable = None.orNull)
+  extends Exception("Expected and actual datasets differ.\n" +
+                    s"Reference path: $refPath\n" +
+                    s"Actual dataset path: $stdPath\n" +
+                    s"Difference written to: $outPath\n" +
+                    s"Count Expected( $expectedCount ) vs Actual( $actualCount )", cause)
+
+final case class CmpJobSchemasDiffer(private val refPath: String,
+                                     private val stdPath: String,
+                                     private val diffSchema: Seq[StructField],
+                                     private val cause: Throwable = None.orNull)
+  extends Exception("Expected and actual datasets differ in schemas.\n"+
+                    s"Reference path: $refPath\n" +
+                    s"Actual dataset path: $stdPath\n" +
+                    s"Difference is $diffSchema", cause)
+
 object ComparisonJob {
-  def getReader(cmd: CmdConfig)(implicit sparkSession: SparkSession): DataFrameReader = {
-    val dfReader = sparkSession.read.format(cmd.rawFormat)
-    // applying format specific options
-    val  dfReader4 = {
-      val dfReader1 = if (cmd.rowTag.isDefined) dfReader.option("rowTag", cmd.rowTag.get) else dfReader
-      val dfReader2 = if (cmd.csvDelimiter.isDefined) dfReader1.option("delimiter", cmd.csvDelimiter.get) else dfReader1
-      val dfReader3 = if (cmd.csvHeader.isDefined) dfReader2.option("header", cmd.csvHeader.get) else dfReader2
-      dfReader3
-    }
-    if (cmd.rawFormat.equalsIgnoreCase("fixed-width")) {
-      val dfReader5 = if (cmd.fixedWidthTrimValues.get) dfReader4.option("trimValues", "true") else dfReader4
-      dfReader5
-    } else dfReader4
-  }
-
-  def removeMetadataFromSchema(schema: StructType): StructType = {
-    StructType(schema.map{ f =>
-      StructField(f.name, f.dataType, f.nullable)
-    })
-  }
-
   def main(args: Array[String]): Unit = {
-    val cmd = CmdConfig.getCmdLineArguments(args)
-    val enableWholeStage = false
+    implicit val cmd: CmdConfig = CmdConfig.getCmdLineArguments(args)
+    val enableWholeStage = false //disable whole stage code gen - the plan is too long
 
     implicit val sparkSession: SparkSession = SparkSession.builder()
       .appName(s"Dataset comparison - '${cmd.stdPath}' and '${cmd.refPath}'")
-      .config("spark.sql.codegen.wholeStage", enableWholeStage) //disable whole stage code gen - the plan is too long
+      .config("spark.sql.codegen.wholeStage", enableWholeStage)
       .getOrCreate()
 
     implicit val sc: SparkContext = sparkSession.sparkContext
 
-    val expectedDf = getReader(cmd).load(cmd.refPath)
-    val actualDf = getReader(cmd).load(cmd.stdPath)
-    val expectedSchema = removeMetadataFromSchema(expectedDf.schema)
-    val actualSchema = removeMetadataFromSchema(actualDf.schema)
+    val expectedDfReader = new DataframeReader(cmd.refPath)
+    val actualDfReader = new DataframeReader(cmd.stdPath)
+    val expectedDf = expectedDfReader.dataFrame
+    val actualDf = actualDfReader.dataFrame
+    val expectedSchema = expectedDfReader.removeMetadataFromSchema
+    val actualSchema = actualDfReader.removeMetadataFromSchema
 
     if (expectedSchema != actualSchema) {
-      System.err.println("Expected and actual datasets differ in schemas.")
-      sys.exit(101) // Place holder error code until we get info from DevOps
+      val diffSchema = actualSchema.diff(expectedSchema) ++ expectedSchema.diff(actualSchema)
+      throw CmpJobSchemasDiffer(cmd.refPath, cmd.stdPath, diffSchema)
     }
 
     val expectedMinusActual: Dataset[Row] = expectedDf.except(actualDf)
@@ -68,8 +72,7 @@ object ComparisonJob {
     if (expectedMinusActual.count() != 0 || actualMinusExpected.count() != 0) {
       expectedMinusActual.write.format("parquet").save(s"${cmd.outPath}/expected_minus_actual")
       actualMinusExpected.write.format("parquet").save(s"${cmd.outPath}/actual_minus_expected")
-      System.err.println(s"Expected and actual datasets differ. Results written to ${cmd.outPath}")
-      sys.exit(102) // Place holder error code until we get info from DevOps
+      throw CmpJobDatasetsDiffer(cmd.refPath, cmd.stdPath, cmd.outPath, expectedDf.count(), actualDf.count())
     } else {
       System.out.println("Expected and actual datasets are the same. Carry On.")
     }
