2365b1941f976835e393ceb53223e0c40e572350
Georgi Chochov <g.chochov@gmail.com>
2019-01-16 10:22:12 +0100

    #104 Clear out unused controller.js methods

--- menas/ui/components/app.controller.js
+++ menas/ui/components/app.controller.js
@@ -71,14 +71,6 @@ sap.ui.define([
     onMappingPress: function (oEv) {
       this._router.navTo("mappingTables")
     },
-    /**
-     * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-     * (NOT before the first rendering! onInit() is used for that one!).
-     * @memberOf menas.main
-     */
-//	onBeforeRendering: function() {
-//
-//	},
 
     /**
      * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
@@ -88,14 +80,6 @@ sap.ui.define([
     onAfterRendering: function () {
       component.setBusy(false)
     },
-
-    /**
-     * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-     * @memberOf menas.main
-     */
-//	onExit: function() {
-//
-//	}
 
   });
 });
--- menas/ui/components/mappingTable/mappingTableMain.controller.js
+++ menas/ui/components/mappingTable/mappingTableMain.controller.js
@@ -454,28 +454,4 @@ sap.ui.controller("components.mappingTable.mappingTableMain", {
       this.fetchSchema();
   }
 
-  /**
-   * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered (NOT before the
-   * first rendering! onInit() is used for that one!).
-   *
-   * @memberOf components.mappingTable.mappingTableMain
-   */
-//onBeforeRendering: function() {
-
-//},
-  /**
-   * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the
-   * HTML could be done here. This hook is the same one that SAPUI5 controls get after being rendered.
-   *
-   * @memberOf components.mappingTable.mappingTableMain
-   */
-
-  /**
-   * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-   *
-   * @memberOf components.mappingTable.mappingTableMain
-   */
-//onExit: function() {
-
-//}
 });
