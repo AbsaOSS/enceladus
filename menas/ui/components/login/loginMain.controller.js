67a6194b3b72e20a1ce3a3942277df9ebd3c206a
Georgi Chochov <g.chochov@gmail.com>
2019-01-16 10:22:12 +0100

    #104 Clear out unused controller.js methods

--- menas/ui/components/login/loginMain.controller.js
+++ menas/ui/components/login/loginMain.controller.js
@@ -105,34 +105,7 @@ sap.ui.define([
         error: fnError
       })
     },
-    /**
-     * Similar to onAfterRendering, but this hook is invoked before the controller's
-     * View is re-rendered (NOT before the first rendering! onInit() is used for
-     * that one!).
-     *
-     * @memberOf components.schema.schemaMain
-     */
-    // onBeforeRendering: function () {
-    //
-    // },
-    /**
-     * Called when the View has been rendered (so its HTML is part of the document).
-     * Post-rendering manipulations of the HTML could be done here. This hook is the
-     * same one that SAPUI5 controls get after being rendered.
-     *
-     * @memberOf components.schema.schemaMain
-     */
-    // onAfterRendering: function() {
-    //
-    // },
-    /**
-     * Called when the Controller is destroyed. Use this one to free resources and
-     * finalize activities.
-     *
-     * @memberOf components.schema.schemaMain
-     */
-    // onExit: function() {
-    //
-    // }
+
   });
+
 });
--- menas/ui/components/schema/schemaMain.controller.js
+++ menas/ui/components/schema/schemaMain.controller.js
@@ -225,33 +225,4 @@ sap.ui.controller("components.schema.schemaMain", {
 
 	}
 
-/**
- * Similar to onAfterRendering, but this hook is invoked before the controller's
- * View is re-rendered (NOT before the first rendering! onInit() is used for
- * that one!).
- *
- * @memberOf components.schema.schemaMain
- */
-// onBeforeRendering: function() {
-//
-// },
-/**
- * Called when the View has been rendered (so its HTML is part of the document).
- * Post-rendering manipulations of the HTML could be done here. This hook is the
- * same one that SAPUI5 controls get after being rendered.
- *
- * @memberOf components.schema.schemaMain
- */
-// onAfterRendering: function() {
-//
-// },
-/**
- * Called when the Controller is destroyed. Use this one to free resources and
- * finalize activities.
- *
- * @memberOf components.schema.schemaMain
- */
-// onExit: function() {
-//
-// }
 });
