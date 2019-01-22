918d9ffd45d93afa48ec8e60c96aba4edf02b4e6
Sasa Zejnilovic <zejnils@gmail.com>
2019-01-22 15:18:06 +0100

    Add a RestTest function to test utils
    - add models folder
    - add TestRun model holding information of one test and all the calls
    from that test
    - add TestCase model holding information about a test case
    - add RestMethod Enum listing all methods used by Menas
    - add TestCaseResult to model holding results of previously mentioned
    TestCases
    - add CmdConfig for a spark job running the rest tests
    - add RestCaller Object which is taking care of choosing the correct
    methods, comparing outputs and meassuring time it took for the test case
    to finish.
    - add RestRunnerJob to run all test in the same way enceladus projects
    are acustomed to. Load data, runs all the rest calls and save evaluation
    to a file. Added to this output is a _TEST_SUMMARY file which saves the
    test name, start and end time, number of failed and passed test cases
    and other information.

---
+++ testutils/src/main/scala/za/co/absa/enceladus/testutils/models/TestRun.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2018-2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.testutils.models
+
+import org.joda.time.{DateTime, DateTimeZone}
+
+import scala.collection.mutable
+
+class TestRun(name: String) {
+  val startTime: DateTime = DateTime.now(DateTimeZone.UTC)
+  lazy val endTime: DateTime = DateTime.now(DateTimeZone.UTC)
+  private[this] val testCaseResults: mutable.MutableList[TestCaseResult] = mutable.MutableList()
+
+  def addTestCaseResult(testCaseResult: TestCaseResult): TestRun = {
+    testCaseResults += testCaseResult
+    this
+  }
+
+  def addTestCaseResults(testCaseResults: Array[TestCaseResult]): TestRun = {
+    testCaseResults.foreach { addTestCaseResult }
+    this
+  }
+
+  def getFinalInfo: String = {
+    val startTimeString = this.startTime.toString()
+    val endTimeString = this.endTime.toString()
+    val numOfPassedUseCases = testCaseResults.count(_.passed)
+    val numOfFailedUseCases = testCaseResults.count(!_.passed)
+    val numOfUseCases = numOfFailedUseCases + numOfPassedUseCases
+
+    val jsonObj = ujson.Obj("name" -> this.name,
+                            "startTime" -> startTimeString,
+                            "endTime" -> endTimeString,
+                            "numberOfUseCases" -> numOfUseCases,
+                            "numberOfPassedUseCases" -> numOfPassedUseCases,
+                            "numberOfFailedUseCases" -> numOfFailedUseCases)
+
+    ujson.write(jsonObj)
+  }
+
+  def getTestCaseResults: List[TestCaseResult] = { testCaseResults.toList }
+}
---
+++ testutils/src/main/scala/za/co/absa/enceladus/testutils/rest/RestCaller.scala
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2018-2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.testutils.rest
+
+import za.co.absa.enceladus.testutils.models.RestMethod._
+import requests.Response
+import gnieh.diffson.sprayJson._
+import za.co.absa.enceladus.testutils.models.{RestMethod, TestCase, TestCaseResult}
+
+import scala.annotation.switch
+
+
+object RestCaller {
+
+  def run(testCase: TestCase): TestCaseResult = {
+    lazy val callOutput = call(testCase.method, testCase.url, testCase.payload, testCase.contentType)
+    val time = calculateTime { callOutput }
+    val comparison = compareJsons(testCase.expectedOutput, callOutput.text)
+    val statusCodeOk = callOutput.statusCode.toString == testCase.expectedStatusCode
+    val statusCodeMessage = s"Should be ${testCase.expectedStatusCode}, is ${callOutput.statusCode}"
+    val passed = comparison.isEmpty && statusCodeOk
+
+    TestCaseResult(testCase.id, testCase.name, comparison, passed, testCase.payload,
+      callOutput.text, testCase.expectedOutput, statusCodeMessage, time)
+  }
+
+  def call(method: RestMethod, url: String, payload: String, contentType: String): Response = {
+    val headers: Map[String, String] = Map("content-type" -> contentType)
+    (method: @switch) match {
+      case RestMethod.POST => call_post(url, payload, headers)
+      case RestMethod.GET => call_get(url, payload, headers)
+      case RestMethod.DELETE => call_delete(url, payload, headers)
+      case RestMethod.PUT => call_put(url, payload, headers)
+    }
+  }
+
+  private def call_post(url: String, payload: String, headers: Map[String, String]): Response = {
+    requests.post(url, data = payload, headers = headers)
+  }
+
+  private def call_get(url: String, payload: String, headers: Map[String, String]): Response = {
+    requests.get(url, data = payload, headers = headers)
+  }
+
+  private def call_delete(url: String, payload: String, headers: Map[String, String]): Response = {
+    requests.delete(url, data = payload, headers = headers)
+  }
+
+  private def call_put(url: String, payload: String, headers: Map[String, String]): Response = {
+    requests.put(url, data = payload, headers = headers)
+  }
+
+  /**
+    * Compares two Strings of JSON standard compliant data
+    * @param expectedJson JSON that is expected
+    * @param actualJson JSON that was returned
+    * @return Returns a diff of the two JSONs
+    */
+  private def compareJsons(expectedJson: String, actualJson: String): String = {
+    JsonDiff.diff(expectedJson, actualJson, remember = false).toString
+  }
+
+  /**
+    * Calculates times that it took for passed block of code to execute and finish.
+    * @param callback Block of code, to be executed
+    * @tparam A A type parameter specifying callback return
+    * @return Returns a millisecond difference between start and end time
+    */
+  private def calculateTime[A](callback: => A): Long = {
+    val startTime = System.nanoTime()
+    callback
+    val endTime = System.nanoTime()
+    endTime - startTime
+  }
+}
