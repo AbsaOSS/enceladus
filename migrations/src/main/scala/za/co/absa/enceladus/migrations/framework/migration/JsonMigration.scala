016598e862599422c7d43140383d85d12749b8bc
Ruslan Iushchenko (PRG) <Ruslan.Iushchenko@absa.co.za>
2019-04-16 15:30:15 +0200

    #311 Add base traits for migrations

---
+++ migrations/src/main/scala/za/co/absa/enceladus/migrations/framework/migration/JsonMigration.scala
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2018-2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.migrations.framework.migration
+
+import scala.collection.mutable
+
+/**
+  * A JsonMigration represents an entity that provides transformations for each document of every collection in a model
+  * when switching from one version of the model to another.
+  *
+  * All transformations are JSON string in, JSON string out.
+  *
+  * Only one transformation is possible per collection.
+  *
+  * In order to create a JSON migration you need to extend from this trait and provide all the requited transformations:
+  *
+  * {{{
+  *   class MigrationTo1 extends JsonMigration {
+  *
+  *     migrate("collection1_name") (jsonIn => {
+  *       val jsonOut = collection1Transformations(jsonIn)
+  *       jsonOut
+  *     })
+  *
+  *     migrate("collection2_name") (jsonIn => {
+  *       val jsonOut = collection2Transformations(jsonIn)
+  *       jsonOut
+  *     })
+  *   }
+  * }}}
+  *
+  */
+trait JsonMigration extends Migration {
+  type DocumentTransformer = String => String
+
+  /**
+    * This function is used by derived classes to add transformations for affected collections.
+    * This is used for complex migrations that requite complex model version maps.
+    *
+    * @param collectionName A collection name to be migrated
+    * @param f              A transformation to applied to each document of the collection
+    *
+    */
+  def migrate(collectionName: String)(f: String => String): Unit = {
+    if (transformers.contains(collectionName)) {
+      throw new IllegalArgumentException(s"A transformer for '$collectionName' has already been added.")
+    }
+    transformers.put(collectionName, f)
+  }
+
+  /**
+    * Gets a JSON transformer for the specified collection if applicable
+    *
+    * @param collectionName A collection name to be migrated
+    * @return A function that takes a JSON string and returns a transformed JSON string
+    *
+    */
+  def getTransformer(collectionName: String): Option[DocumentTransformer] = transformers.get(collectionName)
+
+  private val transformers = new mutable.HashMap[String, DocumentTransformer]()
+}
---
+++ migrations/src/main/scala/za/co/absa/enceladus/migrations/framework/migration/QueryMigration.scala
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2018-2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.migrations.framework.migration
+
+import scala.collection.mutable.ListBuffer
+
+/**
+  * A QueryMigration represents an entity that provides queries to be executed for every affected collections in a model
+  * when switching from one version of the model to another.
+  *
+  * In order to create a query migration you need to extend from this trait and provide all the requited queries:
+  *
+  * {{{
+  *   class MigrationTo1 extends QueryMigration {
+  *
+  *     applyQuery("collection1_name") {
+  *       """
+  *         | db.collection{ $set: { "newField1": "Initial value" } }
+  *         |
+  *       """.stripMargin
+  *     }
+  *
+  *     applyQuery("collection2_name") {
+  *       """
+  *         | db.collection{ $set: { "newField2": "Initial value" } }
+  *         |
+  *       """.stripMargin
+  *     }
+  *   }
+  * }}}
+  *
+  */
+trait QueryMigration extends Migration {
+  type JsQuery = String
+
+  /**
+    * This method is used by derived classes to add queries to be executed on the affected collections.
+    * Use this for quicker migrations like an addition of a column.
+    *
+    * @param collectionName A collection name to be migrated
+    * @param qry            A query to applied to the collection
+    *
+    */
+  def applyQuery(collectionName: String)(qry: => JsQuery): Unit = {
+    queries += collectionName -> qry
+  }
+
+  /**
+    * Gets all queries need to be executed for the specified collection.
+    * The order of the queries corresponds to the order `applyQuery()` method invoked a derived class.
+    *
+    * @param collectionName A collection name to be migrated
+    * @return A string representing a JS query
+    *
+    */
+  def getQueries(collectionName: String): List[JsQuery] = {
+    queries
+      .filter({ case (name, _) => name == collectionName })
+      .map({ case (_, query) => query })
+      .toList
+  }
+
+  private val queries = new ListBuffer[(String, JsQuery)]()
+}
