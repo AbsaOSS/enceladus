efbb4b1a781204934f50ac3b9136a1b43889df64
Dzmitry Makatun <d.i.makatun@gmail.com>
2019-04-30 10:51:51 +0200

    Add filtering by info date interval

--- menas/ui/service/MonitoringService.js
+++ menas/ui/service/MonitoringService.js
@@ -18,9 +18,14 @@ jQuery.sap.require("sap.m.MessageBox");
 var MonitoringService = new function() {
 
   var model = sap.ui.getCore().getModel();
-
   this.getMonitoringPoints = function(sId) {
-    Functions.ajax("api/monitoring/data/datasets/" + encodeURI(sId), "GET", {}, function(oData) {
+    let sStartDate = model.getProperty("/dateFrom")
+    let sEndDate = model.getProperty("/dateTo")
+    Functions.ajax("api/monitoring/data/datasets/"
+      + encodeURI(sId) + "/"
+      + encodeURI(sStartDate) + "/"
+      + encodeURI(sEndDate),
+      "GET", {}, function(oData) {
       model.setProperty("/monitoringPoints", oData)
       let plotLabels = []
 
@@ -47,9 +52,9 @@ var MonitoringService = new function() {
             failedAfterStandardization.push(stdF)
             unprocessedRaw.push(rawTotal - confS - confF)
 
-            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"])
+            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"]+ " datasetVersion: " + elem["datasetVersion"])
             continue
-          } else if (rawTotal != undefined && stdS != undefined && stdF != undefined && confS === undefined && confF === undefined) {
+          } else if (rawTotal != undefined && stdS != undefined && stdF != undefined && confS == undefined && confF == undefined) {
             // only standartization finished
             conformed.push(0)
             failedAfterConformance.push(0)
@@ -58,9 +63,9 @@ var MonitoringService = new function() {
             unprocessedRaw.push(rawTotal - stdS - stdF)
 
 
-            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"])
+            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"] + " datasetVersion: " + elem["datasetVersion"])
             continue
-          } else if (rawTotal != undefined && stdS === undefined && stdF === undefined && confS === undefined && confF === undefined) {
+          } else if (rawTotal != undefined && stdS == undefined && stdF == undefined && confS == undefined && confF == undefined) {
             // only raw data is available
             conformed.push(0)
             failedAfterConformance.push(0)
@@ -68,140 +73,144 @@ var MonitoringService = new function() {
             failedAfterStandardization.push(0)
             unprocessedRaw.push(rawTotal)
 
-            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"])
+            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"]+  " datasetVersion: " + elem["datasetVersion"])
             continue
           } else {
-            // rund data is inconsistent
-            plotLabels.push(elem["informationDate"] + "_v" + elem["reportVersion"] + " ! inconsistent_run_data")
+            // run data is inconsistent
+            plotLabels.push(" ! inconsistent_run_data" + elem["informationDate"] + "_v" + elem["reportVersion"]+ " datasetVersion: " + elem["datasetVersion"])
             continue
           }
 
         }
+
+        let data = {
+          labels: plotLabels,
+          datasets: [
+            {
+              label: "Conformed",
+              fill: true,
+              lineTension: 0.1,
+              backgroundColor: "green",
+              borderColor: "green",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.3,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: conformed,
+              spanGaps: false,
+              stack: "all"
+            },
+            {
+              label: "Failed at conformance",
+              fill: true,
+              lineTension: 0.1,
+              backgroundColor: "red",
+              borderColor: "red",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: failedAfterConformance,
+              spanGaps: false,
+              stack: "all"
+            },
+            {
+              label: "Standartized, not conformed yet",
+              fill: true,
+              lineTension: 0.1,
+              backgroundColor: "blue",
+              borderColor: "blue",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: waitingToConform,
+              spanGaps: false,
+              stack: "all"
+            },
+
+            {
+              label: "Failed at standardization",
+              fill: true,
+              lineTension: 0.1,
+              backgroundColor: "orange",
+              borderColor: "orange",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: failedAfterStandardization,
+              spanGaps: false,
+              stack: "all"
+            },
+
+            {
+              label: "Unprocessed raw",
+              fill: true,
+              lineTension: 0.1,
+              backgroundColor: "black",
+              borderColor: "black",
+              borderCapStyle: 'butt',
+              borderDash: [],
+              borderDashOffset: 0.0,
+              borderJoinStyle: 'miter',
+              pointBorderColor: "rgba(75,192,192,1)",
+              pointBackgroundColor: "#fff",
+              pointBorderWidth: 1,
+              pointHoverRadius: 5,
+              pointHoverBackgroundColor: "rgba(75,192,192,1)",
+              pointHoverBorderColor: "rgba(220,220,220,1)",
+              pointHoverBorderWidth: 2,
+              pointRadius: 1,
+              pointHitRadius: 10,
+              data: unprocessedRaw,
+              spanGaps: false,
+              stack: "all"
+            }
+          ]
+        };
+        model.setProperty("/plotData", data)
+      } else {
+        // no datapoints for this interval
+        model.setProperty("/plotData", [])
+        model.setProperty("/monitoringPoints", [])
       }
-
-      let data = {
-        labels: plotLabels,
-        datasets: [
-          {
-            label: "Conformed",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "green",
-            borderColor: "green",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.3,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: conformed,
-            spanGaps: false,
-            stack: "all"
-          },
-          {
-            label: "Failed at conformance",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "red",
-            borderColor: "red",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: failedAfterConformance,
-            spanGaps: false,
-            stack: "all"
-          },
-          {
-            label: "Waiting for conformance",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "blue",
-            borderColor: "blue",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: waitingToConform,
-            spanGaps: false,
-            stack: "all"
-          },
-
-          {
-            label: "Failed at standardization",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "orange",
-            borderColor: "orange",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: failedAfterStandardization,
-            spanGaps: false,
-            stack: "all"
-          },
-
-          {
-            label: "Unprocessed raw",
-            fill: true,
-            lineTension: 0.1,
-            backgroundColor: "black",
-            borderColor: "black",
-            borderCapStyle: 'butt',
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: 'miter',
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: unprocessedRaw,
-            spanGaps: false,
-            stack: "all"
-          }
-        ]
-      };
-      model.setProperty("/plotData", data)
     }, function() {
       sap.m.MessageBox
         .error("Failed to get monitoring points. Please wait a moment and try reloading the application")
