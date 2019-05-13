b4ec4e71cc43a826206c356edac02280e8194d2e
Sasa Zejnilovic <zejnils@gmail.com>
2019-05-13 22:23:27 +0200

    381 Addressing PR comments
    Add Unit tests
    Remove useless annotations
    Switch complicated function for modulo

---
+++ testutils/src/test/scala/za/co/absa/enceladus/testutils/HelperFunctionsSuite.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2018-2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.testutils
+
+import org.scalatest.FunSuite
+
+class HelperFunctionsSuite extends FunSuite {
+  test("Test basic flow of prettyPrintElapsedTime") {
+    val time = 90061000
+    val response = "1 day, 1 hour, 1 minute and 1.00 second"
+    assert(response == HelperFunctions.prettyPrintElapsedTime(time))
+  }
+
+  test("Test one second prettyPrintElapsedTime") {
+    val time = 1000
+    val response = "1.00 second"
+    assert(response == HelperFunctions.prettyPrintElapsedTime(time))
+  }
+
+  test("Test two hours prettyPrintElapsedTime") {
+    val time = 7200000
+    val response = "2 hours"
+    assert(response == HelperFunctions.prettyPrintElapsedTime(time))
+  }
+
+  test("Test unordered segments prettyPrintElapsedTime") {
+    val time = 432184026
+    val response = "5 days, 3 minutes and 4.03 seconds"
+    assert(response == HelperFunctions.prettyPrintElapsedTime(time))
+  }
+}
