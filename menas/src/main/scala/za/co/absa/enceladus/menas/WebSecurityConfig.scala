38908cee497f27880758cc737b431bea6ad6f775
Jan Scherbaum <kmoj02@gmail.com>
2019-05-30 11:20:49 +0200

    #437 - Menas auth configurable

--- menas/src/main/scala/za/co/absa/enceladus/menas/WebSecurityConfig.scala
+++ menas/src/main/scala/za/co/absa/enceladus/menas/WebSecurityConfig.scala
@@ -42,6 +42,9 @@ import org.springframework.security.web.csrf.CsrfToken
 import org.springframework.security.web.savedrequest.{HttpSessionRequestCache, RequestCache, SavedRequest}
 import org.springframework.stereotype.Component
 import org.springframework.util.StringUtils
+import org.springframework.security.kerberos.authentication.sun.GlobalSunJaasKerberosConfig
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
 
 @EnableWebSecurity
 class WebSecurityConfig {
@@ -65,65 +68,76 @@ class WebSecurityConfig {
   @Value("${za.co.absa.enceladus.menas.auth.inmemory.password}")
   val password: String = ""
 
-//  @Bean
-//  def activeDirectoryLdapAuthenticationProvider() = {
-//    new ActiveDirectoryLdapAuthenticationProvider(adDomain, adServer)
-//  }
-//
-//  @Bean
-//  def spnegoEntryPoint() = {
-//    new SpnegoEntryPoint("/login")
-//  }
-//
-//  @Bean
-//  def spnegoAuthenticationProcessingFilter(authenticationManager: AuthenticationManager) = {
-//    val filter = new SpnegoAuthenticationProcessingFilter()
-//    filter.setAuthenticationManager(authenticationManager)
-//    filter
-//  }
-//
-//  @Bean
-//  def kerberosServiceAuthenticationProvider() = {
-//    val provider = new KerberosServiceAuthenticationProvider()
-//    provider.setTicketValidator(sunJaasKerberosTicketValidator())
-//    provider.setUserDetailsService(ldapUserDetailsService())
-//    provider
-//  }
-//
-//  @Bean
-//  def sunJaasKerberosTicketValidator() = {
-//    val ticketValidator = new SunJaasKerberosTicketValidator()
-//    ticketValidator.setServicePrincipal(servicePrincipal)
-//    ticketValidator.setKeyTabLocation(new FileSystemResource(keytabLocation))
-//    ticketValidator.setDebug(true)
-//    ticketValidator
-//  }
-//
-//  @Bean
-//  def kerberosLdapContextSource() = {
-//    val contextSource = new KerberosLdapContextSource(adServer)
-//    contextSource.setLoginConfig(loginConfig())
-//    contextSource
-//  }
-//
-//  @Bean
-//  def loginConfig() = {
-//    val loginConfig = new SunJaasKrb5LoginConfig()
-//    loginConfig.setKeyTabLocation(new FileSystemResource(keytabLocation))
-//    loginConfig.setServicePrincipal(servicePrincipal)
-//    loginConfig.setDebug(true)
-//    loginConfig.setIsInitiator(true)
-//    loginConfig.afterPropertiesSet()
-//    loginConfig
-//  }
-//
-//  @Bean
-//  def ldapUserDetailsService() = {
-//    val userSearch = new FilterBasedLdapUserSearch(ldapSearchBase, ldapSearchFilter, kerberosLdapContextSource());
-//    val service = new LdapUserDetailsService(userSearch, new ActiveDirectoryLdapAuthoritiesPopulator());
-//    service.setUserDetailsMapper(new LdapUserDetailsMapper())
-//    service
-//  }
+  System.setProperty("javax.net.ssl.trustStore", "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/jre/lib/security/cacerts");
+  System.setProperty("javax.net.debug", "true")
+  System.setProperty("sun.security.krb5.debug", "true")
+
+  @Bean
+  def activeDirectoryLdapAuthenticationProvider() = {
+    val prov = new ActiveDirectoryLdapAuthenticationProvider(adDomain, adServer, ldapSearchBase)
+    prov.setSearchFilter(ldapSearchFilter)
+    prov
+  }
+
+  @Bean
+  def spnegoEntryPoint() = {
+    new SpnegoEntryPoint("/login")
+  }
+
+  @Bean
+  def spnegoAuthenticationProcessingFilter(authenticationManager: AuthenticationManager) = {
+    val filter = new SpnegoAuthenticationProcessingFilter()
+    filter.setAuthenticationManager(authenticationManager)
+    
+    filter
+  }
+
+  @Bean
+  def kerberosServiceAuthenticationProvider() = {
+    val provider = new KerberosServiceAuthenticationProvider()
+    provider.setTicketValidator(sunJaasKerberosTicketValidator())
+    provider.setUserDetailsService(ldapUserDetailsService())
+    provider
+  }
+
+  @Bean
+  def sunJaasKerberosTicketValidator() = {
+    val ticketValidator = new SunJaasKerberosTicketValidator()
+    ticketValidator.setServicePrincipal(servicePrincipal)
+    ticketValidator.setKeyTabLocation(new FileSystemResource(keytabLocation))
+    ticketValidator.setDebug(true)
+    ticketValidator.afterPropertiesSet()
+    ticketValidator
+  }
+
+  @Bean
+  def kerberosLdapContextSource() = {
+    val contextSource = new KerberosLdapContextSource(adServer)
+    contextSource.setLoginConfig(loginConfig())
+//    contextSource.setBase(ldapSearchBase)
+    contextSource.setUserDn("OU=Users,OU=CORP Accounts,DC=corp,DC=dsarena,DC=com")
+    contextSource
+  }
+
+  @Bean
+  def loginConfig() = {
+    val loginConfig = new SunJaasKrb5LoginConfig()
+    loginConfig.setKeyTabLocation(new FileSystemResource(keytabLocation))
+    loginConfig.setServicePrincipal(servicePrincipal)
+    loginConfig.setDebug(true)
+    loginConfig.setIsInitiator(true)
+    loginConfig.afterPropertiesSet()
+    loginConfig
+  }
+
+  @Bean
+  def ldapUserDetailsService() = {
+    val userSearch = new FilterBasedLdapUserSearch(ldapSearchBase, ldapSearchFilter, kerberosLdapContextSource());
+
+    val service = new LdapUserDetailsService(userSearch, new ActiveDirectoryLdapAuthoritiesPopulator());
+    service.setUserDetailsMapper(new LdapUserDetailsMapper())
+    service
+  }
 
   @Bean
   def authenticationFailureHandler(): AuthenticationFailureHandler = {
@@ -137,67 +151,68 @@ class WebSecurityConfig {
 
   @Configuration
   @Order(1)
-  class ApiWebSecurityConfigurationAdapter @Autowired()(restAuthenticationEntyPoint: RestAuthenticationEntryPoint,
-                                                        authenticationSuccessHandler: AuthSuccessHandler,
-                                                        authenticationFailureHandler: AuthenticationFailureHandler,
-                                                        logoutSuccessHandler: LogoutSuccessHandler)
+  class ApiWebSecurityConfigurationAdapter @Autowired() (restAuthenticationEntyPoint: RestAuthenticationEntryPoint,
+      authenticationSuccessHandler: AuthSuccessHandler,
+      authenticationFailureHandler: AuthenticationFailureHandler,
+      logoutSuccessHandler: LogoutSuccessHandler)
     extends WebSecurityConfigurerAdapter {
 
     override def configure(http: HttpSecurity) {
       http
         .csrf()
-        .ignoringAntMatchers("/api/login")
+          .ignoringAntMatchers("/api/login")
         .and()
-        .exceptionHandling()
-        .authenticationEntryPoint(restAuthenticationEntyPoint)
+          .exceptionHandling()
+          .authenticationEntryPoint(restAuthenticationEntyPoint)
         .and()
-        .authorizeRequests()
-        .antMatchers("/index.html", "/resources/**", "/generic/**",
-          "/service/**", "/webjars/**", "/css/**", "/components/**")
-        .permitAll()
-        .anyRequest().authenticated()
+          .authorizeRequests()
+          .antMatchers("/index.html", "/resources/**", "/generic/**",
+            "/service/**", "/webjars/**", "/css/**", "/components/**")
+          .permitAll()
+        .anyRequest()
+          .authenticated()
         .and()
-        .formLogin()
-        .loginProcessingUrl("/api/login")
-        .successHandler(authenticationSuccessHandler)
-        .failureHandler(authenticationFailureHandler)
-        .permitAll()
+          .formLogin()
+          .loginProcessingUrl("/api/login")
+          .successHandler(authenticationSuccessHandler)
+          .failureHandler(authenticationFailureHandler)
+          .permitAll()
         .and()
         .logout()
-        .logoutUrl("/api/logout")
-        .logoutSuccessHandler(logoutSuccessHandler)
-        .permitAll()
-        .clearAuthentication(true)
-        .deleteCookies("JSESSIONID")
-        .invalidateHttpSession(true)
-    }
-
-    override def configure(auth: AuthenticationManagerBuilder) {
-      auth
-        .inMemoryAuthentication()
-          .passwordEncoder(NoOpPasswordEncoder.getInstance())
-          .withUser(username)
-            .password(password)
-            .authorities("ROLE_USER")
+          .logoutUrl("/api/logout")
+          .logoutSuccessHandler(logoutSuccessHandler)
+          .permitAll()
+          .clearAuthentication(true)
+          .deleteCookies("JSESSIONID")
+          .invalidateHttpSession(true)
     }
 
 //    override def configure(auth: AuthenticationManagerBuilder) {
-//      auth.authenticationProvider(activeDirectoryLdapAuthenticationProvider())
-//        .authenticationProvider(kerberosServiceAuthenticationProvider())
+//      auth
+//        .inMemoryAuthentication()
+//        .passwordEncoder(NoOpPasswordEncoder.getInstance())
+//        .withUser(username)
+//        .password(password)
+//        .authorities("ROLE_USER")
 //    }
-//
-//    @Bean
-//    override def authenticationManagerBean() = {
-//      super.authenticationManagerBean()
-//    }
+
+    override def configure(auth: AuthenticationManagerBuilder) {
+       val res = auth.authenticationProvider(kerberosServiceAuthenticationProvider()).authenticationProvider(activeDirectoryLdapAuthenticationProvider())
+       Logger.getRootLogger().setLevel(Level.DEBUG);
+    }
+
+    @Bean
+    override def authenticationManagerBean() = {
+      super.authenticationManagerBean()
+    }
   }
 
   @Component
   class RestAuthenticationEntryPoint extends AuthenticationEntryPoint {
     override def commence(request: HttpServletRequest,
-                          response: HttpServletResponse,
-                          authException: AuthenticationException): Unit = {
-      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+        response: HttpServletResponse,
+        authException: AuthenticationException): Unit = {
+      response.sendError(HttpServletResponse.SC_UNAUTHORIZED, s"Unauthorized")
     }
   }
 
@@ -205,8 +220,8 @@ class WebSecurityConfig {
   class AuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     override def onAuthenticationSuccess(request: HttpServletRequest,
-                                         response: HttpServletResponse,
-                                         authentication: Authentication): Unit = {
+        response: HttpServletResponse,
+        authentication: Authentication): Unit = {
       val csrfToken = request.getAttribute("_csrf").asInstanceOf[CsrfToken]
       response.addHeader(csrfToken.getHeaderName, csrfToken.getToken)
 
