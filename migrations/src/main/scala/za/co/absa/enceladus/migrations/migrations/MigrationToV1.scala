a68fc0de6e99a395d6c4788aa6a008d0face2e34
Ruslan Iushchenko (PRG) <Ruslan.Iushchenko@absa.co.za>
2019-05-31 11:10:46 +0200

    #311 Use Bson serializer to serialize ZonedDateTime as Document

--- migrations/src/main/scala/za/co/absa/enceladus/migrations/migrations/MigrationToV1.scala
+++ migrations/src/main/scala/za/co/absa/enceladus/migrations/migrations/MigrationToV1.scala
@@ -15,13 +15,10 @@
 
 package za.co.absa.enceladus.migrations.migrations
 
-import java.time.ZonedDateTime
-
 import org.apache.log4j.{LogManager, Logger}
 import za.co.absa.enceladus.migrations.framework.migration.{JsonMigration, MigrationBase}
 import za.co.absa.enceladus.migrations.migrations.model0.Serializer0
 import za.co.absa.enceladus.migrations.migrations.model1.Serializer1
-import za.co.absa.enceladus.migrations.migrations.datetime.{DateTimeWrapper, ZonedDateTimeWrapper}
 
 import scala.util.control.NonFatal
 
@@ -42,9 +39,9 @@ object MigrationToV1 extends MigrationBase with JsonMigration {
         schema0.name,
         schema0.version,
         None,
-        dateCreated = ZonedDateTimeWrapper.fromZonedDateTime(ZonedDateTime.now()),
-        lastUpdated = ZonedDateTimeWrapper.fromZonedDateTime(ZonedDateTime.now()),
         userCreated = "migration",
+        userUpdated = "migration",
+        disabled = false,
         fields = schema0.fields.map(convertSchemaField(_, Nil))
       )
 
