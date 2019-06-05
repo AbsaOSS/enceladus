84f5c9778f5a9d550bfdcae411889e24711f33ba
Ruslan Iushchenko (PRG) <Ruslan.Iushchenko@absa.co.za>
2019-06-05 10:26:56 +0200

    #445 Add support for default default value in mapping tables
    If a target attribute of default value mapping is set to '*', the expression
    will be used if there is no default value a target attribute selected in
    the mapping rule.

--- conformance/src/test/scala/za/co/absa/enceladus/samples/TradeConformance.scala
+++ conformance/src/test/scala/za/co/absa/enceladus/samples/TradeConformance.scala
@@ -17,7 +17,7 @@ package za.co.absa.enceladus.samples
 
 import java.io.File
 
-import org.apache.commons.io.FileUtils
+import org.apache.commons.io.{FileUtils, IOUtils}
 import org.apache.log4j.{LogManager, Logger}
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import za.co.absa.enceladus.model.conformanceRule._
@@ -35,7 +35,8 @@ object TradeConformance {
     schemaName = "currency", schemaVersion = 0, defaultMappingValue = List(DefaultValue("currency_name", "\"Unknown\"")))
 
   val productMT = MappingTable(name = "product", version = 0, hdfsPath = "src/test/testData/_productMT",
-    schemaName = "product", schemaVersion = 0)
+    schemaName = "product", schemaVersion = 0, defaultMappingValue =
+      List(DefaultValue("product_name", "\"Unknown1\""), DefaultValue("*", "\"Unknown2\"")))
 
   val countryRule = MappingConformanceRule(order = 1, mappingTable = "country", controlCheckpoint = true,
     mappingTableVersion = 0, attributeMappings = Map("country_code" -> "legs.conditions.country"),
@@ -54,96 +55,29 @@ object TradeConformance {
     mappingTableVersion = 0, attributeMappings = Map("product_code" -> "legs.conditions.product"),
     targetAttribute = "product_name", outputColumn = "legs.conditions.conformed_product")
 
+  val product2Rule = MappingConformanceRule(order = 6, mappingTable = "product", controlCheckpoint = true,
+    mappingTableVersion = 0, attributeMappings = Map("product_code" -> "legs.conditions.product"),
+    targetAttribute = "product_name2", outputColumn = "legs.conditions.conformed_product_new")
 
-  val lit2Rule = LiteralConformanceRule(order = 6, outputColumn = "ToBeDropped", controlCheckpoint = true,
+  val lit2Rule = LiteralConformanceRule(order = 7, outputColumn = "ToBeDropped", controlCheckpoint = true,
     value = "123456")
 
-  val dropRule = DropConformanceRule(order = 7, outputColumn = "ToBeDropped", controlCheckpoint = false)
+  val dropRule = DropConformanceRule(order = 8, outputColumn = "ToBeDropped", controlCheckpoint = false)
 
-  val concatRule = ConcatenationConformanceRule(order = 8, outputColumn = "Concatenated", controlCheckpoint = true,
+  val concatRule = ConcatenationConformanceRule(order = 9, outputColumn = "Concatenated", controlCheckpoint = true,
     Seq("MyLiteral", "MyUpperLiteral"))
 
-  val singleColRule = SingleColumnConformanceRule(order = 9, outputColumn = "legs.ConformedLegId",
+  val singleColRule = SingleColumnConformanceRule(order = 10, outputColumn = "legs.ConformedLegId",
     controlCheckpoint = true, inputColumn = "legs.legid", inputColumnAlias = "legId")
 
   val tradeDS = Dataset(name = "Trade Conformance", version = 1, hdfsPath = "src/test/testData/_tradeData",
     hdfsPublishPath = "testData/conformedTrade",
     schemaName = "Employee", schemaVersion = 0,
-    conformance = List(countryRule, litRule, upperRule, currencyRule, productRule, lit2Rule, dropRule,
+    conformance = List(countryRule, litRule, upperRule, currencyRule, productRule, product2Rule, lit2Rule, dropRule,
       concatRule, singleColRule))
 
-  val tradeInfoFile: String =
-    """{
-      |  "metadata": {
-      |    "sourceApplication": "Test Data",
-      |    "country": "ZA",
-      |    "historyType": "Snapshot",
-      |    "dataFilename": "tradeData_20170721.dat",
-      |    "sourceType": "Golden",
-      |    "version": 1,
-      |    "informationDate": "01-01-2017",
-      |    "additionalInfo": {
-      |      "key1": "value1",
-      |      "key2": "value2"
-      |    }
-      |  },
-      |  "checkpoints": [
-      |    {
-      |      "name": "Source",
-      |      "processStartTime": "01-01-2017 08:00:00",
-      |      "processEndTime": "01-01-2017 08:00:00",
-      |      "workflowName": "Source",
-      |      "order": 1,
-      |      "controls": [
-      |        {
-      |          "controlName": "totalCount",
-      |          "controlType": "controlType.count",
-      |          "controlCol": "*",
-      |          "controlValue": "7"
-      |        },
-      |        {
-      |          "controlName": "countId",
-      |          "controlType": "controlType.distinctCount",
-      |          "controlCol": "id",
-      |          "controlValue": "7"
-      |        },
-      |        {
-      |          "controlName": "sumId",
-      |          "controlType": "controlType.aggregatedTotal",
-      |          "controlCol": "id",
-      |          "controlValue": "28"
-      |        }
-      |      ]
-      |    },
-      |    {
-      |      "name": "Raw",
-      |      "processStartTime": "01-01-2017 08:00:00",
-      |      "processEndTime": "01-01-2017 08:00:00",
-      |      "workflowName": "Raw",
-      |      "order": 2,
-      |      "controls": [
-      |        {
-      |          "controlName": "totalCount",
-      |          "controlType": "controlType.count",
-      |          "controlCol": "*",
-      |          "controlValue": "7"
-      |        },
-      |        {
-      |          "controlName": "countId",
-      |          "controlType": "controlType.distinctCount",
-      |          "controlCol": "id",
-      |          "controlValue": "7"
-      |        },
-      |        {
-      |          "controlName": "sumId",
-      |          "controlType": "controlType.aggregatedTotal",
-      |          "controlCol": "id",
-      |          "controlValue": "28"
-      |        }
-      |      ]
-      |    }
-      |  ]
-      |}""".stripMargin
+  val tradeInfoFile: String = IOUtils.toString(this.getClass
+    .getResourceAsStream("/interpreter/tradeConformance/TradeInfoFile.json"), "UTF-8")
 
   // The original sample used to create the parquet file
   // import spark.implicits._
@@ -153,28 +87,16 @@ object TradeConformance {
   """{"id":1,"legs":[{"legid":100,"conditions":[{"checks":[{"checkNums":["1","2","3b","4","5c","6"]}],"country":"CZE","currency":"Kc","product":"Stock"}]}]}""" ::
     """{"id":2,"legs":[{"legid":200,"conditions":[{"checks":[{"checkNums":["8","9","10b","11","12c","13"]}],"country":"SA","currency":"Rand","product":"Stock"}]}]}""" ::
     """{"id":3,"legs":[{"legid":300,"conditions":[{"checks":[],"country": "SWE","currency":"SWK","product":"Stock"}]},{"legid":301,"conditions":[{"checks":[],"country": "SA","currency":"Dummy","product":"Bond"}]}]}""" ::
-    """{"id":4,"legs":[{"legid":400,"conditions":[{"checks":null,"country": "CZE","currency":"Kc","product":"Bond"}]},{"legid":401,"conditions":[{"checks":null,"country": "SA","currency":"Rand","product":"Stock"}]}]}""" ::
+    """{"id":4,"legs":[{"legid":400,"conditions":[{"checks":null,"country": "CZE","currency":"Kc","product":"Wrong"}]},{"legid":401,"conditions":[{"checks":null,"country": "SA","currency":"Rand","product":"Stock"}]}]}""" ::
     """{"id":5,"legs":[{"legid":500,"conditions":[]},{"legid":501,"conditions":[]},{"legid":502,"conditions":[]}]}""" ::
     """{"id":6,"legs":[]}""" ::
     """{"id":7}""" :: Nil
 
-  val expectedConformedGroupExplode: List[String] =
-    """{"id":1,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"legs":[{"conditions":[{"checks":[{"checkNums":["1","2","3b","4","5c","6"]}],"country":"CZE","currency":"Kc","product":"Stock","conformed_country":"Czech Republic","conformed_currency":"CZK","conformed_product":"STK"}],"legid":100,"ConformedLegId":{"legId":100}}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":2,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"legs":[{"conditions":[{"checks":[{"checkNums":["8","9","10b","11","12c","13"]}],"country":"SA","currency":"Rand","product":"Stock","conformed_country":"South Africa","conformed_currency":"ZAR","conformed_product":"STK"}],"legid":200,"ConformedLegId":{"legId":200}}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":3,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_country","rawValues":["SWE"],"mappings":[{"mappingTableColumn":"country_code","mappedDatasetColumn":"legs.conditions.country"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_currency","rawValues":["Dummy"],"mappings":[{"mappingTableColumn":"currency_code","mappedDatasetColumn":"legs.conditions.currency"}]}],"legs":[{"conditions":[{"checks":[],"country":"SWE","currency":"SWK","product":"Stock","conformed_currency":"SEK","conformed_product":"STK"}],"legid":300,"ConformedLegId":{"legId":300}},{"conditions":[{"checks":[],"country":"SA","currency":"Dummy","product":"Bond","conformed_country":"South Africa","conformed_currency":"Unknown","conformed_product":"BND"}],"legid":301,"ConformedLegId":{"legId":301}}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":4,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"legs":[{"conditions":[{"country":"CZE","currency":"Kc","product":"Bond","conformed_country":"Czech Republic","conformed_currency":"CZK","conformed_product":"BND"}],"legid":400,"ConformedLegId":{"legId":400}},{"conditions":[{"country":"SA","currency":"Rand","product":"Stock","conformed_country":"South Africa","conformed_currency":"ZAR","conformed_product":"STK"}],"legid":401,"ConformedLegId":{"legId":401}}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":5,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"legs":[{"conditions":[],"legid":500,"ConformedLegId":{"legId":500}},{"conditions":[],"legid":501,"ConformedLegId":{"legId":501}},{"conditions":[],"legid":502,"ConformedLegId":{"legId":502}}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":6,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"legs":[],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":7,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"Concatenated":"abcdefABCDEF"}""" :: Nil
+  val expectedConformedGroupExplode: String = IOUtils.toString(this.getClass
+    .getResourceAsStream("/interpreter/tradeConformance/expectedConformedGroupExplode.json"), "UTF-8")
 
-  val expectedConformedJson: List[String] =
-    """{"id":1,"legs":[{"conditions":[{"checks":[{"checkNums":["1","2","3b","4","5c","6"]}],"country":"CZE","currency":"Kc","product":"Stock","conformed_country":"Czech Republic","conformed_currency":"CZK","conformed_product":"STK"}],"legid":100,"ConformedLegId":{"legId":100}}],"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":2,"legs":[{"conditions":[{"checks":[{"checkNums":["8","9","10b","11","12c","13"]}],"country":"SA","currency":"Rand","product":"Stock","conformed_country":"South Africa","conformed_currency":"ZAR","conformed_product":"STK"}],"legid":200,"ConformedLegId":{"legId":200}}],"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":3,"legs":[{"conditions":[{"checks":[],"country":"SWE","currency":"SWK","product":"Stock","conformed_currency":"SEK","conformed_product":"STK"}],"legid":300,"ConformedLegId":{"legId":300}},{"conditions":[{"checks":[],"country":"SA","currency":"Dummy","product":"Bond","conformed_country":"South Africa","conformed_currency":"Unknown","conformed_product":"BND"}],"legid":301,"ConformedLegId":{"legId":301}}],"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_country","rawValues":["SWE"],"mappings":[{"mappingTableColumn":"country_code","mappedDatasetColumn":"legs.conditions.country"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_currency","rawValues":["Dummy"],"mappings":[{"mappingTableColumn":"currency_code","mappedDatasetColumn":"legs.conditions.currency"}]}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":4,"legs":[{"conditions":[{"country":"CZE","currency":"Kc","product":"Bond","conformed_country":"Czech Republic","conformed_currency":"CZK","conformed_product":"BND"}],"legid":400,"ConformedLegId":{"legId":400}},{"conditions":[{"country":"SA","currency":"Rand","product":"Stock","conformed_country":"South Africa","conformed_currency":"ZAR","conformed_product":"STK"}],"legid":401,"ConformedLegId":{"legId":401}}],"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":5,"legs":[{"conditions":[],"legid":500,"ConformedLegId":{"legId":500}},{"conditions":[],"legid":501,"ConformedLegId":{"legId":501}},{"conditions":[],"legid":502,"ConformedLegId":{"legId":502}}],"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_country","rawValues":[null],"mappings":[{"mappingTableColumn":"country_code","mappedDatasetColumn":"legs.conditions.country"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_currency","rawValues":[null],"mappings":[{"mappingTableColumn":"currency_code","mappedDatasetColumn":"legs.conditions.currency"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_product","rawValues":[null],"mappings":[{"mappingTableColumn":"product_code","mappedDatasetColumn":"legs.conditions.product"}]}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":6,"legs":[],"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_country","rawValues":[null],"mappings":[{"mappingTableColumn":"country_code","mappedDatasetColumn":"legs.conditions.country"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_currency","rawValues":[null],"mappings":[{"mappingTableColumn":"currency_code","mappedDatasetColumn":"legs.conditions.currency"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_product","rawValues":[null],"mappings":[{"mappingTableColumn":"product_code","mappedDatasetColumn":"legs.conditions.product"}]}],"Concatenated":"abcdefABCDEF"}""" ::
-      """{"id":7,"MyLiteral":"abcdef","MyUpperLiteral":"ABCDEF","errCol":[{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_country","rawValues":[null],"mappings":[{"mappingTableColumn":"country_code","mappedDatasetColumn":"legs.conditions.country"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_currency","rawValues":[null],"mappings":[{"mappingTableColumn":"currency_code","mappedDatasetColumn":"legs.conditions.currency"}]},{"errType":"confMapError","errCode":"E00001","errMsg":"Conformance Error - Null produced by mapping conformance rule","errCol":"legs.conditions.conformed_product","rawValues":[null],"mappings":[{"mappingTableColumn":"product_code","mappedDatasetColumn":"legs.conditions.product"}]}],"Concatenated":"abcdefABCDEF"}""" :: Nil
+  val expectedConformedJson: String = IOUtils.toString(this.getClass
+    .getResourceAsStream("/interpreter/tradeConformance/expectedConformed.json"), "UTF-8")
 
   def createTestDataFiles()(implicit spark: SparkSession): Unit = {
     createTradeData()
@@ -210,7 +132,9 @@ object TradeConformance {
 
   def createProductMT()(implicit spark: SparkSession): Unit = {
     import spark.implicits._
-    Seq(("Stock", "STK"), ("Bond", "BND")).toDF.select($"_1".as("product_code"), $"_2".as("product_name"))
+    Seq(("Stock", "STK", "STK2"), ("Bond", "BND", "BND2"))
+      .toDF
+      .select($"_1".as("product_code"), $"_2".as("product_name"), $"_3".as("product_name2"))
          .write.mode(SaveMode.Overwrite).parquet(getMappingTablePath(productMT.hdfsPath))
   }
 
