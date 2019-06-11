a1d35e76c7a77b626baf6ec72fb6c0d5ae1d9c20
Jan Scherbaum <kmoj02@gmail.com>
2019-06-11 09:36:01 +0200

    #456 Only display the warning at login page

--- menas/ui/components/app.controller.js
+++ menas/ui/components/app.controller.js
@@ -105,10 +105,6 @@ sap.ui.define([
 
     onAfterRendering: function () {
       component.setBusy(false);
-
-      if(!sap.ui.Device.browser.chrome || (sap.ui.Device.browser.chrome && sap.ui.Device.browser.version < 68)) {
-        sap.m.MessageBox.warning("Chrome (version 68 and higher) is currently the only supported browser for menas");
-      }
     }
 
   });
