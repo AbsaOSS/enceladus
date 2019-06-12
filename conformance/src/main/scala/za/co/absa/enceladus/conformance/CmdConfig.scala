986a1eb2055790b89dbde5e5693af250a65fa1af
Ruslan Iushchenko (PRG) <Ruslan.Iushchenko@absa.co.za>
2019-06-12 10:49:52 +0200

    #445 Fix PR suggestions

--- conformance/src/main/scala/za/co/absa/enceladus/conformance/CmdConfig.scala
+++ conformance/src/main/scala/za/co/absa/enceladus/conformance/CmdConfig.scala
@@ -88,16 +88,16 @@ object CmdConfig {
       )
 
     opt[String]("performance-file").optional().action((value, config) =>
-      config.copy(performanceMetricsFile = Some(value))).text("Produce a performance metrics file at the given location (local filesystem)")
+      config.copy(performanceMetricsFile = Option(value))).text("Produce a performance metrics file at the given location (local filesystem)")
 
     opt[String]("debug-set-publish-path").optional().hidden().action((value, config) =>
-      config.copy(publishPathOverride = Some(value))).text("override the path of the published data (used internally for testing)")
+      config.copy(publishPathOverride = Option(value))).text("override the path of the published data (used internally for testing)")
 
     opt[String]("folder-prefix").optional().action((value, config) =>
-      config.copy(folderPrefix = Some(value))).text("Adds a folder prefix before the infoDateColumn")
+      config.copy(folderPrefix = Option(value))).text("Adds a folder prefix before the infoDateColumn")
 
     opt[Boolean]("experimental-mapping-rule").optional().action((value, config) =>
-      config.copy(experimentalMappingRule = Some(value))).text("Use experimental optimized mapping conformance rule")
+      config.copy(experimentalMappingRule = Option(value))).text("Use experimental optimized mapping conformance rule")
 
     help("help").text("prints this usage text")
   }
--- conformance/src/main/scala/za/co/absa/enceladus/conformance/DynamicConformanceJob.scala
+++ conformance/src/main/scala/za/co/absa/enceladus/conformance/DynamicConformanceJob.scala
@@ -82,7 +82,7 @@ object DynamicConformanceJob {
     val inputData = DataSource.getData(stdPath, dateTokens(0), dateTokens(1), dateTokens(2), "")
     // perform the conformance
     val result: DataFrame = try {
-      DynamicInterpreter.interpret(conformance, inputData, getExperimentalRuleEnabled(), enableCF)
+      DynamicInterpreter.interpret(conformance, inputData, isExperimentalRuleEnabled(), enableCF)
     }
     catch {
       case e: ValidationException =>
@@ -97,10 +97,16 @@ object DynamicConformanceJob {
     processResult(result, performance, publishPath, stdPath)
   }
 
-  private def getExperimentalRuleEnabled()(implicit cmd: CmdConfig): Boolean = {
+  private def isExperimentalRuleEnabled()(implicit cmd: CmdConfig): Boolean = {
+    val configPath = "conformance.mapping.rule.experimental.implementation"
     val enabled = cmd.experimentalMappingRule match {
       case Some(b) => b
-      case None => conf.getBoolean("conformance.mapping.rule.experimental.implementation")
+      case None =>
+        if (conf.hasPath(configPath)) {
+          conf.getBoolean(configPath)
+        } else {
+          false
+        }
     }
     log.info(s"Experimental mapping rule enabled = $enabled")
     enabled
