14a0968ba193758a6c60af3a8a3ce6d22dcfbc0e
Georgi Chochov <g.chochov@gmail.com>
2019-07-04 10:28:26 +0200

    #603 Ensure /runs GET requests are backward comaptible

--- menas/ui/service/RunService.js
+++ menas/ui/service/RunService.js
@@ -18,7 +18,7 @@ jQuery.sap.require("sap.m.MessageBox");
 var RunService = new function () {
 
   this.getRuns = function (oMasterPage) {
-    Functions.ajax("api/runs", "GET", {},
+    Functions.ajax("api/runs/summaries", "GET", {},
       oData => {
         this._bindRunSummaries(oData, oMasterPage);
       },
@@ -29,7 +29,7 @@ var RunService = new function () {
   };
 
   this.getFirstRun = function(oControl, oTable) {
-    Functions.ajax("api/runs", "GET", {},
+    Functions.ajax("api/runs/summaries", "GET", {},
       oData => {
         if (oData.length > 0 && oControl) {
           let firstDataset = oData[0];
@@ -66,7 +66,7 @@ var RunService = new function () {
   };
 
   this.getLatestRun = function (oControl, oTable, sDatasetName, sDatasetVersion) {
-    Functions.ajax("api/runs/" + encodeURI(sDatasetName) + "/" + encodeURI(sDatasetVersion) + "/latest", "GET", {},
+    Functions.ajax("api/runs/" + encodeURI(sDatasetName) + "/" + encodeURI(sDatasetVersion) + "/latestrun", "GET", {},
       oData => {
         this.setCurrentRun(oControl, oTable, oData);
       },
@@ -77,7 +77,7 @@ var RunService = new function () {
   };
 
   this.getLatestRunForLatestVersion = function (oControl, oTable, datasetName) {
-    Functions.ajax("api/runs/" + encodeURI(datasetName) + "/latest", "GET", {},
+    Functions.ajax("api/runs/" + encodeURI(datasetName) + "/latestrun", "GET", {},
       oData => {
         this.setCurrentRun(oControl, oTable, oData);
       },
@@ -116,7 +116,7 @@ var RunService = new function () {
     let info = oRun.controlMeasure.metadata.additionalInfo;
     oRun.controlMeasure.metadata.additionalInfo = this._mapAdditionalInfo(info);
 
-    oRun.status = Formatters.statusToPrettyString(oRun.runStatus.status.value);
+    oRun.status = Formatters.statusToPrettyString(oRun.runStatus.status);
 
     const sStdName = this._nameExists(aCheckpoints, "Standardization Finish") ? "Standardization Finish" : "Standardization - End";
 
