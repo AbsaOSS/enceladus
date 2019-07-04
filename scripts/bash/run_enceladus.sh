1593428c71da810a91b16033e0d20bfe42c10f6f
Ruslan Iushchenko <yruslan@gmail.com>
2019-07-04 13:11:39 +0200

    #597 Address PR comments
    (Thanks @Zejnilovic)

--- scripts/bash/run_enceladus.sh
+++ scripts/bash/run_enceladus.sh
@@ -19,9 +19,10 @@
 DRY_RUN=""
 
 # Command line defaults for 'spark-submit'
+MASTER="yarn"
+DEPLOY_MODE="client"
 NUM_EXECUTORS=""
 EXECUTOR_MEMORY="4G"
-DEPLOY_MODE="client"
 DRIVER_CORES="4"
 DRIVER_MEMORY="8G"
 
@@ -36,6 +37,7 @@ DELIMITER=""
 HEADER=""
 TRIM_VALUES=""
 MAPPING_TABLE_PATTERN="''"
+FOLDER_PREFIX=""
 
 # Security command line defaults
 MENAS_CREDENTIALS_FILE=""
@@ -54,100 +56,89 @@ case $key in
     ;;
     --num-executors)
     NUM_EXECUTORS="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --executor-memory)
     EXECUTOR_MEMORY="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
+    ;;
+    --master)
+    MASTER="$2"
+    shift 2 # past argument and value
     ;;
     --deploy-mode)
     DEPLOY_MODE="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --driver-cores)
     DRIVER_CORES="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --driver-memory)
     DRIVER_MEMORY="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --jar)
     JAR="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --class)
     JAR="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
-    --dataset-name)
+    -D|--dataset-name)
     DATASET_NAME="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
-    --dataset-version)
+    -d|--dataset-version)
     DATASET_VERSION="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
-    --report-date)
+    -R|--report-date)
     REPORT_DATE="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
-    --report-version)
+    -r|--report-version)
     REPORT_VERSION="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
+    ;;
+    --folder-prefix)
+    FOLDER_PREFIX="$2"
+    shift 2 # past argument and value
     ;;
 
-    --raw-format)
+    -f|--raw-format)
     RAW_FORMAT="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;  
     --rowtag)
     ROWTAG="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --delimiter)
     DELIMITER="\\$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --header)
     HEADER="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --trimValues)
     TRIM_VALUES="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --mapping-table-pattern)
     MAPPING_TABLE_PATTERN="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
 
     --menas-credentials-file)
     MENAS_CREDENTIALS_FILE="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     --menas-auth-keytab)
     MENAS_AUTH_KEYTAB="$2"
-    shift # past argument
-    shift # past value
+    shift 2 # past argument and value
     ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
@@ -194,10 +185,20 @@ CONF="spark.driver.extraJavaOptions=-Dmenas.rest.uri=$MENAS_URI -Dstandardized.h
 -Dspline.mongodb.url=$SPLINE_MONGODB_URL -Dspline.mongodb.name=$SPLINE_MONGODB_NAME -Dhdp.version=2.7.3 \
 -Dconformance.mappingtable.pattern=$MAPPING_TABLE_PATTERN"
 
-MASTER="--master yarn --deploy-mode $DEPLOY_MODE"
-RESOURCES="--num-executors $NUM_EXECUTORS --executor-memory $EXECUTOR_MEMORY --driver-cores $DRIVER_CORES --driver-memory $DRIVER_MEMORY"
-CMD_LINE="$SPARK_SUBMIT $MASTER $RESOURCES --conf \"$CONF\" --class $CLASS $JAR"
+CMD_LINE="$SPARK_SUBMIT"
 
+# Adding command line parameters that go BEFORE the jar file
+add_to_cmd_line "--master" ${MASTER}
+add_to_cmd_line "--deploy-mode" ${DEPLOY_MODE}
+add_to_cmd_line "--num-executors" ${NUM_EXECUTORS}
+add_to_cmd_line "--executor-memory" ${EXECUTOR_MEMORY}
+add_to_cmd_line "--driver-cores" ${DRIVER_CORES}
+add_to_cmd_line "--driver-memory" ${DRIVER_MEMORY}
+
+# Adding JVM configuration, entry point class name and the jar file
+CMD_LINE="$CMD_LINE --conf \"$CONF\" --class $CLASS $JAR"
+
+# Adding command line parameters that go AFTER the jar file
 add_to_cmd_line "--dataset-name" ${DATASET_NAME}
 add_to_cmd_line "--dataset-version" ${DATASET_VERSION}
 add_to_cmd_line "--report-date" ${REPORT_DATE}
@@ -207,6 +208,7 @@ add_to_cmd_line "--rowtag" ${ROWTAG}
 add_to_cmd_line "--delimiter" ${DELIMITER}
 add_to_cmd_line "--header" ${HEADER}
 add_to_cmd_line "--trimValues" ${TRIM_VALUES}
+add_to_cmd_line "--folder-prefix" ${FOLDER_PREFIX}
 
 echo "Command line:"
 echo "$CMD_LINE"
