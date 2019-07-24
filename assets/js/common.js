87125199b125f77d8879a85f94837e1fb89eafbc
Sasa Zejnilovic <zejnils@gmail.com>
2019-07-24 14:55:56 +0200

    #696 Add github pages template
    Addressing comments, removing leftovers and fixing style
    
    Thank you @GeorgiChochov and @leborgneromain

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,5 +2,5 @@
 ---
 
 function switchVersion(selectedOption, urlPostfix) {
-  window.location.href = "{{ site.url }}{{ site.baseurl }}/docs/" + selectedOption + "/" + urlPostfix;
+  window.location.href = `{{ site.url }}{{ site.baseurl }}/docs/${selectedOption}/${urlPostfix}`;
 }
