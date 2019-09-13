ca3f51e264410c39f7edcade3956456442d7fcf6
Jan Scherbaum <kmoj02@gmail.com>
2019-09-13 09:43:37 +0200

    #491 Refactor

--- menas/ui/components/dataset/conformanceRule/SchemaManager.js
+++ menas/ui/components/dataset/conformanceRule/SchemaManager.js
@@ -16,13 +16,13 @@
 class SchemaManager {
 
   static getTransitiveSchemas(schemas, rules) {
-    if(rules) {
-      rules.map(RuleFactory.createRule).forEach((rule, index) => {
-        const schema = $.extend(true, [], schemas[index]);
-        rule.apply(schema.fields);
-        schemas.push(schema);
-      })
-    }
+    if(!rules) return;
+
+    rules.map(RuleFactory.createRule).forEach((rule, index) => {
+      const schema = $.extend(true, [], schemas[index]);
+      rule.apply(schema.fields);
+      schemas.push(schema);
+    })
   }
 
   static validateTransitiveSchemas(firstSchema, rules) {
