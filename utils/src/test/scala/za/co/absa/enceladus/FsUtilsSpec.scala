ab80bc1f2bef1fcabae1de5179a78bff3077779c
Ruslan Iushchenko <yruslan@gmail.com>
2019-10-11 09:15:59 +0200

    #968 Group file system unit tests to make them more readable.

--- utils/src/test/scala/za/co/absa/enceladus/FsUtilsSpec.scala
+++ utils/src/test/scala/za/co/absa/enceladus/FsUtilsSpec.scala
@@ -18,95 +18,98 @@ package za.co.absa.enceladus
 import java.io.FileNotFoundException
 
 import org.apache.hadoop.fs.Path
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 import za.co.absa.enceladus.utils.fs.FileSystemVersionUtils
 import za.co.absa.enceladus.utils.testUtils.SparkTestBase
 
 /**
   * Unit tests for File system utils
   */
-class FsUtilsSpec extends FlatSpec with Matchers with SparkTestBase {
+class FsUtilsSpec extends WordSpec with Matchers with SparkTestBase {
   val fsUtils = new FileSystemVersionUtils(spark.sparkContext.hadoopConfiguration)
 
-  "splitUriPath" should "split URI and path" in
-  {
-    val path = new Path("hdfs://some-host:8020/user/data/input")
-    val (prefix, rawPath) = fsUtils.splitUriPath(path)
-    prefix shouldEqual "hdfs://some-host:8020"
-    rawPath shouldEqual "/user/data/input"
-  }
+  "splitUriPath" should {
+    "split URI and path" in {
+      val path = new Path("hdfs://some-host:8020/user/data/input")
+      val (prefix, rawPath) = fsUtils.splitUriPath(path)
+      prefix shouldEqual "hdfs://some-host:8020"
+      rawPath shouldEqual "/user/data/input"
+    }
 
-  "splitUriPath" should "split not split a path without URI prefix" in
-    {
+    "not split a path without URI prefix" in {
       val path = new Path("/projects/coreconformance/publish/dataset")
       val (prefix, rawPath) = fsUtils.splitUriPath(path)
       prefix shouldEqual ""
       rawPath shouldEqual "/projects/coreconformance/publish/dataset"
     }
 
-  "splitUriPath" should "split not split relative path" in
-    {
+    "not split relative path" in {
       val path = new Path("data/input")
       val (prefix, rawPath) = fsUtils.splitUriPath(path)
       prefix shouldEqual ""
       rawPath shouldEqual "data/input"
     }
+  }
 
-  "getDirectorySize" should "throw an exception if provided path does not exist" in {
-    val e = intercept[FileNotFoundException] {
-      fsUtils.getDirectorySize("src/test/resources/test_data/not_exist")
+  "getDirectorySize" should {
+    "throw an exception if the specified path does not exist" in {
+      intercept[FileNotFoundException] {
+        fsUtils.getDirectorySize("src/test/resources/test_data/not_exist")
+      }
+    }
+
+    "return the file size if a single file is specified" in {
+      val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir/dummy.txt")
+      assert(dirSize == 20L)
+    }
+
+    "return the file size if a single hidden file is specified" in {
+      val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir/_hidden_dummy.txt")
+      assert(dirSize == 27L)
+    }
+
+    "return the size of all files in a directory" in {
+      val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir")
+      assert(dirSize == 47L)
+    }
+
+    "return the size of all files recursively" in {
+      val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir2")
+      assert(dirSize == 87L)
     }
   }
 
-  "getDirectorySize" should "return the file size if a single file is specified" in {
-    val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir/dummy.txt")
-    assert(dirSize == 20L)
-  }
-
-  "getDirectorySize" should "return the file size if a single hidden file is specified" in {
-    val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir/_hidden_dummy.txt")
-    assert(dirSize == 27L)
-  }
-
-  "getDirectorySize" should "return the size of all files in a directory" in {
-    val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir")
-    assert(dirSize == 47L)
-  }
-
-  "getDirectorySize" should "return the size of all files recursively" in {
-    val dirSize = fsUtils.getDirectorySize("src/test/resources/test_data/test_dir2")
-    assert(dirSize == 87L)
-  }
-
-  "getDirectorySizeNoHidden" should "throw an exception if provided path does not exist" in {
-    val e = intercept[FileNotFoundException] {
-      fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/not_exist")
+  "getDirectorySizeNoHidden" should {
+    "throw an exception if the specified path does not exist" in {
+      intercept[FileNotFoundException] {
+        fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/not_exist")
+      }
     }
-  }
 
-  "getDirectorySizeNoHidden" should "return the file size if a single file is specified" in {
-    val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir/dummy.txt")
-    assert(dirSize == 20L)
-  }
+    "return the file size if a single file is specified" in {
+      val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir/dummy.txt")
+      assert(dirSize == 20L)
+    }
 
-  "getDirectorySizeNoHidden" should "return the file size if a single hidden file is specified" in {
-    val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir/_hidden_dummy.txt")
-    assert(dirSize == 27L)
-  }
+    "return the file size if a single hidden file is specified" in {
+      val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir/_hidden_dummy.txt")
+      assert(dirSize == 27L)
+    }
 
-  "getDirectorySizeNoHidden" should "return the size of all non-hidden files in a directory" in {
-    val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir")
-    assert(dirSize == 20L)
-  }
+    "return the size of all non-hidden files in a directory" in {
+      val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir")
+      assert(dirSize == 20L)
+    }
 
-  "getDirectorySizeNoHidden" should "return the size of all non-hidden files recursively along non-hidden paths" in {
-    val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir2")
-    assert(dirSize == 40L)
-  }
+    "return the size of all non-hidden files recursively along non-hidden paths" in {
+      val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir2")
+      assert(dirSize == 40L)
+    }
 
-  "getDirectorySizeNoHidden" should "return the size of all non-hidden files if a hidden directory is specified explicitly" in {
-    val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir2/_inner_dir")
-    assert(dirSize == 20L)
+    "return the size of all non-hidden files if a hidden directory is specified explicitly" in {
+      val dirSize = fsUtils.getDirectorySizeNoHidden("src/test/resources/test_data/test_dir2/_inner_dir")
+      assert(dirSize == 20L)
+    }
   }
 
 }
