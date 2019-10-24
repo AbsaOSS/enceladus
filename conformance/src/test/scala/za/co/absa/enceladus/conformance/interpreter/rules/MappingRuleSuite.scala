08a8d43622ed2bb9270cd5907aa37b4e941be0f2
Ruslan Yushchenko <yruslan@gmail.com>
2019-10-24 15:10:37 +0200

    #748 Add a test that demonstrates the issue.

---
+++ conformance/src/test/scala/za/co/absa/enceladus/conformance/interpreter/rules/MappingRuleSuite.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2018-2019 ABSA Group Limited
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package za.co.absa.enceladus.conformance.interpreter.rules
+
+import org.apache.spark.sql.AnalysisException
+import org.scalatest.{BeforeAndAfterAll, FunSuite}
+import za.co.absa.enceladus.conformance.interpreter.DynamicInterpreter
+import za.co.absa.enceladus.conformance.interpreter.rules.datasetfactories.SimpleDatasetFactory
+import za.co.absa.enceladus.utils.testUtils.{LoggerTestBase, SparkTestBase}
+
+class MappingRuleSuite extends FunSuite with SparkTestBase with LoggerTestBase with BeforeAndAfterAll {
+  private val ruleName = "Mapping rule"
+  private val columnName = "dummy"
+
+  private val exampleFactory = new SimpleDatasetFactory()
+
+  override def beforeAll(): Unit = {
+    super.beforeAll()
+    exampleFactory.createMappingTables()
+  }
+
+  override def afterAll(): Unit = {
+    exampleFactory.deleteMappingTables()
+    super.afterAll()
+  }
+
+  test("Test empty mapping table error handling in a mapping rule") {
+    implicit val (inputDf, dataset, dao, progArgs, featureSwitches) =
+      exampleFactory.createExample(true, exampleFactory.emptyTableMappingRule)
+
+    val ex = intercept[AnalysisException] {
+      DynamicInterpreter.interpret(dataset, inputDf).cache
+    }
+
+    assert(ex.getMessage().contains("Unable to infer schema for Parquet"))
+  }
+
+  test("Test empty mapping table error handling in an experimental mapping rule") {
+    implicit val (inputDf, dataset, dao, progArgs, featureSwitches) =
+      exampleFactory.createExample(false, exampleFactory.emptyTableMappingRule)
+
+    val ex = intercept[AnalysisException] {
+      DynamicInterpreter.interpret(dataset, inputDf).cache
+    }
+
+    assert(ex.getMessage().contains("Unable to infer schema for Parquet"))
+  }
+
+}
