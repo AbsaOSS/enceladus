04b7614facf1c27d6530062d177c1a15bd240176
Adrian Olosutean <adrian.olosutean@absa.africa>
2020-04-22 19:16:44 +0200

    #1293 Enforce minimum 2.4.3 Spark version for HyperConformance

--- spark-jobs/src/main/scala/za/co/absa/enceladus/conformance/HyperConformance.scala
+++ spark-jobs/src/main/scala/za/co/absa/enceladus/conformance/HyperConformance.scala
@@ -19,16 +19,20 @@ import java.text.SimpleDateFormat
 import java.util.Date
 
 import org.apache.commons.configuration2.Configuration
+import org.apache.spark.SPARK_VERSION
 import org.apache.spark.sql.functions.{lit, to_date}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.slf4j.{Logger, LoggerFactory}
 import za.co.absa.enceladus.common.Constants._
+import za.co.absa.enceladus.common.SparkCompatibility
+import za.co.absa.enceladus.common.version.SparkVersionGuard
 import za.co.absa.enceladus.conformance.interpreter.{Always, DynamicInterpreter, FeatureSwitches}
 import za.co.absa.enceladus.dao.MenasDAO
 import za.co.absa.enceladus.dao.auth.{MenasCredentialsFactory, MenasKerberosCredentialsFactory, MenasPlainCredentialsFactory}
 import za.co.absa.enceladus.dao.rest.{MenasConnectionStringParser, RestDaoFactory}
 import za.co.absa.hyperdrive.ingestor.api.transformer.{StreamTransformer, StreamTransformerFactory}
-
+import za.co.absa.commons.version.Version._
+import za.co.absa.commons.version.impl.SemVer20Impl.SemanticVersion
 
 class HyperConformance (implicit cmd: ConfCmdConfig,
                         featureSwitches: FeatureSwitches,
@@ -93,10 +97,14 @@ object HyperConformance extends StreamTransformerFactory with HyperConformanceAt
   val log: Logger = LoggerFactory.getLogger(this.getClass)
 
   private val defaultReportVersion = 1
+  private val minimumSparkVersion: SemanticVersion = semver"2.4.3"
 
   @throws[IllegalArgumentException]
   override def apply(conf: Configuration): StreamTransformer = {
     log.info("Building HyperConformance")
+
+    SparkVersionGuard(minimumSparkVersion, SparkCompatibility.maxSparkVersionExcluded)
+      .ensureSparkVersionCompatibility(SPARK_VERSION)
 
     validateConfiguration(conf)
 
@@ -125,7 +133,6 @@ object HyperConformance extends StreamTransformerFactory with HyperConformanceAt
       .setBroadcastMaxSizeMb(0)
 
     implicit val menasBaseUrls: List[String] = MenasConnectionStringParser.parse(conf.getString(menasUriKey))
-
     new HyperConformance()
   }
 
