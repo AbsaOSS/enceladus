91a37d8aa6c772590a9c52ada9a504c205df2d39
Ruslan Iushchenko <yruslan@gmail.com>
2020-06-10 18:02:04 +0200

    #1385 Switch spark-jobs to use 'reference.conf'. Log the effective configuration.

--- utils/src/main/scala/za/co/absa/enceladus/utils/general/ConfigReader.scala
+++ utils/src/main/scala/za/co/absa/enceladus/utils/general/ConfigReader.scala
@@ -15,10 +15,14 @@
 
 package za.co.absa.enceladus.utils.general
 
-import com.typesafe.config.{Config, ConfigFactory}
+import com.typesafe.config.{Config, ConfigFactory, ConfigObject, ConfigValue}
+import org.slf4j.LoggerFactory
+
+import scala.collection.mutable
 
 object ConfigReader {
   private val config: Config = ConfigFactory.load()
+  private val log = LoggerFactory.getLogger(this.getClass)
 
   def readStringConfigIfExist(path: String): Option[String] = {
     if (config.hasPath(path)) {
@@ -30,5 +34,65 @@ object ConfigReader {
 
   def readStringConfig(path: String, default: String): String = {
     readStringConfigIfExist(path).getOrElse(default)
+  }
+
+  /**
+   * Flattens TypeSafe config tree and returns the effective configuration
+   * while redacting sensitive keys.
+   *
+   * @param sensitiveKeys A set of keys for which values shouldn't be logged.
+   * @return the effective configuration as a map
+   */
+  def getFlatConfig(sensitiveKeys: Set[String] = Set()): Map[String, String] = {
+    import collection.JavaConverters._
+
+    val flatEntries = mutable.Map[String, String]()
+
+    def redact(key: String, value: String): String = {
+      if (sensitiveKeys.contains(key)) {
+        "******"
+      } else {
+        value
+      }
+    }
+
+    def render(path: String, obj: ConfigObject): Unit = {
+      obj.asScala.foreach({ case (key, value) =>
+        val flatKey = if (path.isEmpty) {
+          key
+        } else {
+          s"$path.$key"
+        }
+
+        value match {
+          case c: ConfigObject =>
+            render(flatKey, c)
+          case v: ConfigValue =>
+            val redactedValue = redact(flatKey, v.render())
+            flatEntries += flatKey -> redactedValue
+        }
+      })
+    }
+
+    render("", config.root())
+    flatEntries.toMap
+  }
+
+  /**
+   * Logs the effective configuration while redacting sensitive keys.
+   *
+   * @param sensitiveKeys A set of keys for which values shouldn't be logged.
+   */
+  def logEffectiveConfig(sensitiveKeys: Set[String] = Set()): Unit = {
+    val flatConfig = getFlatConfig(sensitiveKeys)
+    val renderedConfig = flatConfig
+      .toSeq
+      .sortBy(_._1)
+      .map {
+        case (k, v) => s"$k = $v"
+      }
+      .mkString("\n")
+
+    log.info(s"Effective configuration:\n$renderedConfig")
   }
 }
