addc5a0b59b59bf0f230a91c44b758364959b516
David Benedeki <benedeki@volny.cz>
2020-06-26 15:49:18 +0200

    1414: Add release notes for v2.8.0 * Grouped items by module first * Imported ghpages scripts update https://github.com/AbsaOSS/gh-pages-skeleton/issues/12

--- utils/get_release_notes/issue.rb
+++ utils/get_release_notes/issue.rb
@@ -32,7 +32,7 @@ class Issue
       # First line of a comment can start by blanks (spaces, tabs), then Release Notes eaither
       # capitalized or all lower case. Then any combination of blanks, dashes or columns followed
       # by a new line. The first line is going to be removed, so the new line is important.
-      comment[:body] =~ /\A[[:blank:]]*(R|r)elease (N|n)otes([[:blank:]]|-|:)*(\r)?\n/
+      comment[:body] =~ /\A[[:blank:]]*(R|r)elease[[:blank:]]+(N|n)otes([[:blank:]]|-|:)*(\r)?\n/
     end
 
     if release_notes.nil? || release_notes.empty?
