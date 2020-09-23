5879a4476758dfa1d67a262d589b607b1a945372
Daniel Kavan <dk1844@gmail.com>
2020-09-23 14:44:52 +0200

    testrun update

--- utils/src/main/scala/za/co/absa/enceladus/utils/fs/example/S3FsUtilsTestJob.scala
+++ utils/src/main/scala/za/co/absa/enceladus/utils/fs/example/S3FsUtilsTestJob.scala
@@ -27,9 +27,10 @@ import za.co.absa.enceladus.utils.fs.S3FsUtils
 object S3FsUtilsTestJob {
 
   private val log = LoggerFactory.getLogger(this.getClass)
+  private val bucketName = "putYourBucketBucketNameHere"
 
   def main(args: Array[String]): Unit = {
-    val basePath ="s3://euw1-ctodatadev-dev-bigdatarnd-s3-poc/ao-hdfs-data/hdfs_data/std/std_nf_dn"
+    val basePath = s"s3://$bucketName/ao-hdfs-data/hdfs_data/std/std_nf_dn"
 
     val sparkBuilder = SparkSession.builder().appName("Sample S3 Measurements 1 Job")
     val spark = sparkBuilder
@@ -45,25 +46,26 @@ object S3FsUtilsTestJob {
       override val maxKeys = 5 // to test recursive listing/action
     }
 
-    log.info(s"dir size of $basePath is:" +  s3utils.getDirectorySize(basePath))
-    log.info(s"dir size (no hidden) of $basePath is:" +  s3utils.getDirectorySizeNoHidden(basePath))
+    log.info(s"dir size of $basePath is:" + s3utils.getDirectorySize(basePath))
+    log.info(s"dir size (no hidden) of $basePath is:" + s3utils.getDirectorySizeNoHidden(basePath))
 
-    log.info(s"should exist:" +  s3utils.exists(s"$basePath/1/2019/11/27/1/_INFO"))
-    log.info(s"should not exist:" +  s3utils.exists(s"$basePath/1/2019/11/27/1/_INFOxxx"))
+    log.info(s"should exist:" + s3utils.exists(s"$basePath/1/2019/11/27/1/_INFO"))
+    log.info(s"should not exist:" + s3utils.exists(s"$basePath/1/2019/11/27/1/_INFOxxx"))
 
     log.info("found version (1): "
-      + s3utils.getLatestVersion("s3://euw1-ctodatadev-dev-bigdatarnd-s3-poc/superhero/publish", "2020-08-06"))
+      + s3utils.getLatestVersion(s"s3://$bucketName/superhero/publish", "2020-08-06"))
 
     log.info("found no version (0): "
-      + s3utils.getLatestVersion("s3://euw1-ctodatadev-dev-bigdatarnd-s3-poc/aaa", "2020-08-06"))
+      + s3utils.getLatestVersion(s"s3://$bucketName/aaa", "2020-08-06"))
 
-    log.info(s"reading file content:" +  s3utils.read(s"$basePath/1/2019/11/27/1/_INFO").take(50))
+    log.info(s"reading file content:" + s3utils.read(s"$basePath/1/2019/11/27/1/_INFO").take(50))
 
-    log.info(s"should find no gz-s:" +  s3utils.isNonSplittable("s3://euw1-ctodatadev-dev-bigdatarnd-s3-poc/gz-list/nogz"))
-    log.info(s"should find some gz-s (and breakOut):" +  s3utils.isNonSplittable("s3://euw1-ctodatadev-dev-bigdatarnd-s3-poc/gz-list/somegz"))
+    log.info(s"should find no gz-s:" + s3utils.isNonSplittable(s"s3://$bucketName/gz-list/nogz"))
+    log.info(s"should find some gz-s (and breakOut):" +
+      s3utils.isNonSplittable(s"s3://$bucketName/gz-list/somegz"))
 
-    val deletePath = "s3://euw1-ctodatadev-dev-bigdatarnd-s3-poc/delete"
-    log.info(s"deleting $deletePath" +  s3utils.deleteDirectoryRecursively(deletePath))
+    val deletePath = s"s3://$bucketName/delete"
+    log.info(s"deleting $deletePath: " + s3utils.deleteDirectoryRecursively(deletePath))
   }
 
 }
