c49e2f97195bd86c37cbe71e9cc74f60fb7b96ef
Adrian Olosutean <adrian.olosutean@absa.africa>
2020-10-26 13:57:34 +0200

    #1554 server.xml cleanup

--- menas/src/main/resources/start_menas.sh
+++ menas/src/main/resources/start_menas.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "$PRIVATE_KEY" >> conf/private.pem
-echo "$CERTIFICATE" >> conf/certificate.pem
-echo "$CA_CHAIN" >> conf/cachain.pem
+echo "${PRIVATE_KEY}" >> conf/private.pem
+echo "${CERTIFICATE}" >> conf/certificate.pem
+echo "${CA_CHAIN}" >> conf/cachain.pem
 catalina.sh run
