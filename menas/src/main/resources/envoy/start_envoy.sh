40bfdf926a036aaef7bdc5e5bfe99842ae2770c1
Adrian Olosutean <adrian.olosutean@absa.africa>
2020-11-02 15:12:53 +0200

    #1554 Added envoy config + enabling running unencrypted container

---
+++ menas/src/main/resources/envoy/start_envoy.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /etc/ssl
+
+echo -e "$PRIVATE_KEY" >> /etc/ssl/envoy.key
+echo -e "$CERTIFICATE" >> /etc/ssl/envoy.crt
+echo -e "$CA_CHAIN" >> /etc/ssl/envoy.crt
+
+/usr/local/bin/envoy -c /etc/envoy.yaml
