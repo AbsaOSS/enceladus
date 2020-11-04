2ccce21a0d41d3c725e6d50f7d91dfaf206ec680
Sasa Zejnilovic <zejnils@gmail.com>
2020-11-04 14:32:44 +0100

    #1592 Helper scripts don't ignore unknown options

--- scripts/bash/enceladus_env.template.sh
+++ scripts/bash/enceladus_env.template.sh
@@ -100,3 +100,6 @@ ADDITIONAL_JVM_CONF_CLIENT="$APPLICATION_PROPERTIES_CLIENT $JAAS_CLIENT"
 # Avoid suppression of Info level logger. This will lead to the fact that, we are not able to get application_id
 # and thus the scripts will not be able to continue properly, not giving the status update or kill option on interrupt
 ADDITIONAL_JVM_CONF_CLUSTER="$APPLICATION_PROPERTIES_CLUSTER $JAAS_CLUSTER"
+
+# Switch that tells the script if it should exit if it encounters unrecognized.
+EXIT_ON_UNRECOGNIZED_OPTIONS="true"
