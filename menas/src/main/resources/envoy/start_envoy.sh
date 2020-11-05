0a59bd8d4c28c11f0c6a32d7157c0ad65996ac0a
Adrian Olosutean <adi.olosutean@gmail.com>
2020-11-05 12:50:59 +0100

    Update menas/src/main/resources/envoy/start_envoy.sh
    Co-authored-by: Saša Zejnilović <zejnils@gmail.com>

--- menas/src/main/resources/envoy/start_envoy.sh
+++ menas/src/main/resources/envoy/start_envoy.sh
@@ -2,8 +2,8 @@
 
 mkdir -p /etc/ssl
 
-echo -e "$PRIVATE_KEY" >> /etc/ssl/envoy.key
-echo -e "$CERTIFICATE" >> /etc/ssl/envoy.crt
-echo -e "$CA_CHAIN" >> /etc/ssl/envoy.crt
+echo -e "${PRIVATE_KEY}" >> /etc/ssl/envoy.key
+echo -e "${CERTIFICATE}" >> /etc/ssl/envoy.crt
+echo -e "${CA_CHAIN}" >> /etc/ssl/envoy.crt
 
 /usr/local/bin/envoy -c /etc/envoy.yaml
