6dec360e73562211c1cb0cbbef18b06c5be8b657
Adrian Olosutean <adrian.olosutean@absa.africa>
2021-01-18 15:07:18 +0200

    #1612 Working TLS on nginx, resources not included in code

--- menas-web/start_menas_web.sh
+++ menas-web/start_menas_web.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [ -n "${PRIVATE_KEY}" ] && [ -n "${CERTIFICATE}" ]; then
+  envsubst < ./nginx.conf > /etc/nginx/nginx.conf
+  echo "${PRIVATE_KEY}" >> /etc/ssl/private.pem
+  echo "${CERTIFICATE}" >> /etc/ssl/certificate.pem
+  echo "${CA_CHAIN}" >> /etc/ssl/cachain.pem
+fi
+
 envsubst < /usr/share/nginx/html/package.json > /usr/share/nginx/html/package-new.json
 rm /usr/share/nginx/html/package.json
 mv /usr/share/nginx/html/package-new.json /usr/share/nginx/html/package.json
