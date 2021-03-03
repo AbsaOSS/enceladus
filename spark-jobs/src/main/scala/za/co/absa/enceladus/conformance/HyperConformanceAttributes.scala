1b0cfd4eabbcd5e63de0ec171f83488edf8abfe4
Daniel Kavan <dk1844@gmail.com>
2021-03-03 15:58:05 +0100

    #1706 scala 2.12: buildable (even with -Pintegration)  - hyperdrive disabled, spline 0.5 for Scala 2.12 & Spark 2.4 used - deserves revisiting as in PR#1175  - dependency for org.apache.commons:commons-configuration2 added, because it is brought by hyperdrive transitively.
     was not run, though.

--- spark-jobs/src/main/scala/za/co/absa/enceladus/conformance/HyperConformanceAttributes.scala
+++ spark-jobs/src/main/scala/za/co/absa/enceladus/conformance/HyperConformanceAttributes.scala
@@ -1,67 +1,69 @@
-/*
- * Copyright 2018 ABSA Group Limited
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+//todo reenable with Hyperdrive API for Scala 2.12 #1712
 
-package za.co.absa.enceladus.conformance
-
-import za.co.absa.hyperdrive.ingestor.api.{HasComponentAttributes, PropertyMetadata}
-
-object HyperConformanceAttributes {
-
-  // Configuration keys expected to be set up when running Conformance as a Transformer component for Hyperdrive
-  val menasUriKey = "menas.rest.uri"
-  val menasCredentialsFileKey = "menas.credentials.file"
-  val menasAuthKeytabKey = "menas.auth.keytab"
-  val datasetNameKey = "dataset.name"
-  val datasetVersionKey = "dataset.version"
-  val reportDateKey = "report.date"
-  val reportVersionKey = "report.version"
-  val eventTimestampColumnKey = "event.timestamp.column"
-  val eventTimestampPatternKey = "event.timestamp.pattern"
-}
-
-trait HyperConformanceAttributes extends HasComponentAttributes {
-  import HyperConformanceAttributes._
-
-  override def getName: String = "HyperConformance"
-
-  override def getDescription: String = "Dynamic Conformance - a transformer component for Hyperdrive"
-
-  override def getProperties: Map[String, PropertyMetadata] = Map(
-    menasUriKey ->
-      PropertyMetadata("Menas API URL", Some("E.g. http://localhost:8080/menas"), required = true),
-    datasetNameKey ->
-      PropertyMetadata("Dataset name", None, required = true),
-    datasetVersionKey ->
-      PropertyMetadata("Dataset version", None, required = true),
-    reportDateKey ->
-      PropertyMetadata("Report date", Some("The current date is used by default0"), required = false),
-    reportVersionKey ->
-      PropertyMetadata("Report version", Some("Will be determined automatically by default if not specified"), required = false),
-    eventTimestampColumnKey ->
-      PropertyMetadata("Event timestamp column",
-        Some("If specified, the column will be used to generate 'enceladus_info_date'"),
-        required = false),
-    eventTimestampPatternKey ->
-      PropertyMetadata("Event timestamp pattern",
-        Some("If the event timestamp column is string, the pattern is used to parse it (default is 'yyyy-MM-dd'T'HH:mm'Z''"),
-        required = false),
-    menasCredentialsFileKey ->
-      PropertyMetadata("Menas credentials file", Some("Relative or absolute path to the file on local FS or HDFS"), required = false),
-    menasAuthKeytabKey ->
-      PropertyMetadata("Keytab", Some("Relative or absolute path to the file on local FS or HDFS"), required = false)
-  )
-
-  override def getExtraConfigurationPrefix: Option[String] = None
-}
+///*
+// * Copyright 2018 ABSA Group Limited
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *     http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//package za.co.absa.enceladus.conformance
+//
+//import za.co.absa.hyperdrive.ingestor.api.{HasComponentAttributes, PropertyMetadata}
+//
+//object HyperConformanceAttributes {
+//
+//  // Configuration keys expected to be set up when running Conformance as a Transformer component for Hyperdrive
+//  val menasUriKey = "menas.rest.uri"
+//  val menasCredentialsFileKey = "menas.credentials.file"
+//  val menasAuthKeytabKey = "menas.auth.keytab"
+//  val datasetNameKey = "dataset.name"
+//  val datasetVersionKey = "dataset.version"
+//  val reportDateKey = "report.date"
+//  val reportVersionKey = "report.version"
+//  val eventTimestampColumnKey = "event.timestamp.column"
+//  val eventTimestampPatternKey = "event.timestamp.pattern"
+//}
+//
+//trait HyperConformanceAttributes extends HasComponentAttributes {
+//  import HyperConformanceAttributes._
+//
+//  override def getName: String = "HyperConformance"
+//
+//  override def getDescription: String = "Dynamic Conformance - a transformer component for Hyperdrive"
+//
+//  override def getProperties: Map[String, PropertyMetadata] = Map(
+//    menasUriKey ->
+//      PropertyMetadata("Menas API URL", Some("E.g. http://localhost:8080/menas"), required = true),
+//    datasetNameKey ->
+//      PropertyMetadata("Dataset name", None, required = true),
+//    datasetVersionKey ->
+//      PropertyMetadata("Dataset version", None, required = true),
+//    reportDateKey ->
+//      PropertyMetadata("Report date", Some("The current date is used by default0"), required = false),
+//    reportVersionKey ->
+//      PropertyMetadata("Report version", Some("Will be determined automatically by default if not specified"), required = false),
+//    eventTimestampColumnKey ->
+//      PropertyMetadata("Event timestamp column",
+//        Some("If specified, the column will be used to generate 'enceladus_info_date'"),
+//        required = false),
+//    eventTimestampPatternKey ->
+//      PropertyMetadata("Event timestamp pattern",
+//        Some("If the event timestamp column is string, the pattern is used to parse it (default is 'yyyy-MM-dd'T'HH:mm'Z''"),
+//        required = false),
+//    menasCredentialsFileKey ->
+//      PropertyMetadata("Menas credentials file", Some("Relative or absolute path to the file on local FS or HDFS"), required = false),
+//    menasAuthKeytabKey ->
+//      PropertyMetadata("Keytab", Some("Relative or absolute path to the file on local FS or HDFS"), required = false)
+//  )
+//
+//  override def getExtraConfigurationPrefix: Option[String] = None
+//}
