184c3d9f5b180b55a0c1fb7e193e8d3870a56362
Sasa Zejnilovic <zejnils@gmail.com>
2021-03-04 14:40:08 +0100

    #601 Swagger 2 PoC

--- menas/src/main/scala/za/co/absa/enceladus/menas/SpringFoxConfig.scala
+++ menas/src/main/scala/za/co/absa/enceladus/menas/SpringFoxConfig.scala
@@ -1,11 +1,25 @@
 package za.co.absa.enceladus.menas
 
+import com.google.common.base.Predicate
 import org.springframework.context.annotation.{Bean, Configuration}
-import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import com.google.common.base.Predicates.or
+import springfox.documentation.builders.PathSelectors.regex
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import com.google.common.base.Predicate
+import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.builders.PathSelectors.regex
+import com.google.common.base.Predicates.or
+
 
 @Configuration
 @EnableSwagger2
@@ -15,7 +29,10 @@ class SpringFoxConfig {
     new Docket(DocumentationType.SWAGGER_2)
       .select
       .apis(RequestHandlerSelectors.any)
-      .paths(PathSelectors.any)
+      .paths(filteredPaths)
       .build
   }
+
+  private def filteredPaths: Predicate[String] =
+    or[String](regex("/api/dataset.*"), regex("/api/schema.*"))
 }
