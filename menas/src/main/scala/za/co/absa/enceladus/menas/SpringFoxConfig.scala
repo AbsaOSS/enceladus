17e3ad74e8e8529350a7eb7b6e43ae24f073e2ce
Sasa Zejnilovic <zejnils@gmail.com>
2021-03-04 16:57:07 +0100

    #601 Swagger 2 PoC

--- menas/src/main/scala/za/co/absa/enceladus/menas/SpringFoxConfig.scala
+++ menas/src/main/scala/za/co/absa/enceladus/menas/SpringFoxConfig.scala
@@ -1,25 +1,16 @@
 package za.co.absa.enceladus.menas
 
-import com.google.common.base.Predicate
-import org.springframework.context.annotation.{Bean, Configuration}
 import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
-import springfox.documentation.swagger2.annotations.EnableSwagger2
-import com.google.common.base.Predicates.or
-import springfox.documentation.builders.PathSelectors.regex
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import com.google.common.base.Predicate
-import springfox.documentation.builders.ApiInfoBuilder
-import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2
 import springfox.documentation.builders.PathSelectors.regex
 import com.google.common.base.Predicates.or
-
+import springfox.documentation.builders.ApiInfoBuilder
 
 @Configuration
 @EnableSwagger2
@@ -27,6 +18,7 @@ class SpringFoxConfig {
   @Bean
   def api(): Docket = {
     new Docket(DocumentationType.SWAGGER_2)
+      .apiInfo(apiInfo)
       .select
       .apis(RequestHandlerSelectors.any)
       .paths(filteredPaths)
@@ -35,4 +27,12 @@ class SpringFoxConfig {
 
   private def filteredPaths: Predicate[String] =
     or[String](regex("/api/dataset.*"), regex("/api/schema.*"))
+
+  private def apiInfo =
+    new ApiInfoBuilder()
+      .title("Menas API")
+      .description("Menas API reference for developers")
+      .license("Apache 2.0 License")
+      .version("1.0") // api or project?
+      .build
 }
