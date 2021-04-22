009b4d09ea9bb9e4d9c76a7a1a3412c960881cc2
David Benedeki <benedeki@volny.cz>
2021-04-22 21:49:18 +0200

    #1763: Helper scripts missing description of new switches * --dra-num-executors description * --dra-executor-cores description * --dra-executor-memory description

--- scripts/cmd/_print_help.cmd
+++ scripts/cmd/_print_help.cmd
@@ -29,9 +29,12 @@ ECHO   --dry-run          Show spark-submit command line without actually runnin
 ECHO/
 ECHO spark-specific-options:
 ECHO !!!WARNING - set these only if you know what you are doing. This could for example disable DRA
-ECHO   --num-executors NUM                      Number of executors to launch.
-ECHO   --executor-cores NUM                     Number of cores per executor.
-ECHO   --executor-memory MEM                    Memory per executor (e.g. 1000M, 2G) (Default: 1G).
+ECHO   --num-executors NUM                      Number of executors to launch. Effective only if DRA is off.
+ECHO   --executor-cores NUM                     Number of cores per executor. Effective only if DRA is off.
+ECHO   --executor-memory MEM                    Memory per executor (e.g. 1000M, 2G) (Default: 1G). Effective only if DRA is off.
+ECHO   --dra-num-executors NUM                  Same as '--num-executors' but used when DRA is enabled. Use with care!
+ECHO   --dra-executor-cores NUM                 Same as '--executor-memory' but used when DRA is enabled.
+ECHO   --dra-executor-memory MEM                Same as '--executor-cores' but used when DRA is enabled.
 ECHO   --master MASTER_URL                      spark://host:port, mesos://host:port, yarn, k8s://https://host:port, or local
 ECHO   --deploy-mode DEPLOY_MODE                Whether to launch the driver program locally ("client") or on one of the worker machines inside the cluster ("cluster").
 ECHO   --driver-cores NUM                       Number of cores used by the driver, only in cluster mode.
