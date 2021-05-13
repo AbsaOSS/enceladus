3d5e7a119874fbe3d47f07c55dda7abe26e1326f
Adrian Olosutean <adi.olosutean@gmail.com>
2021-05-13 14:25:22 +0200

    fix2

--- menas/src/main/resources/docker/start_menas.sh
+++ menas/src/main/resources/docker/start_menas.sh
@@ -19,7 +19,7 @@ DETECTED_HOSTNAME=$(hostname)
 DETECTED_IP=$(echo $DETECTED_HOSTNAME | cut -d"." -f 1 | sed -e 's/-/./g' | cut -c 4-)
 echo -e "\n\nDETECTED_IP=$DETECTED_IP\nDETECTED_HOSTNAME=$DETECTED_HOSTNAME\n\n"
 echo -e "Current file contents:\n $(cat /etc/hosts)"
-echo "\n$DETECTED_IP menas-fargate-elb.ctodatadev.aws.dsarena.com" >> /etc/hosts
+echo "$DETECTED_IP menas-fargate-elb.ctodatadev.aws.dsarena.com" >> /etc/hosts
 echo -e "\n\n\nUpdated file contents:\n $(cat /etc/hosts)"
 CMD="$@"
 $CMD
