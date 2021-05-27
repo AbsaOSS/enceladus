3cea54338921ac96d27074ac305411b512ea618b
Daniel Kavan <dk1844@gmail.com>
2021-05-27 11:28:08 +0200

    #1784 - PR update: replaceBlankProperties on FE redone in functional way

--- menas/ui/service/EntityService.js
+++ menas/ui/service/EntityService.js
@@ -325,9 +325,9 @@ class DatasetService extends EntityService {
     }
   }
 
-  // if _properties with empty string exist, they will be replaced with undefs in place (backend expecting an empty option)
-  replaceBlankProperties(dataset) {
-    const updatedProperties = dataset._properties.map((oProp) => {
+  // if properties with empty string value exist, the returned properties copy will not contain these objects (backend expecting an empty option)
+  stripBlankProperties(properties) {
+    const updatedProperties = properties.map((oProp) => {
       if (oProp.value === "") {
         const changedProp = jQuery.extend({}, oProp);
         changedProp.value = undefined;
@@ -336,11 +336,12 @@ class DatasetService extends EntityService {
         return oProp; // unchanged
       }
     });
-    dataset._properties = updatedProperties; // changed in place
+
+    return updatedProperties;
   }
 
   update(oDataset) {
-    this.replaceBlankProperties(oDataset);
+    oDataset._properties = this.stripBlankProperties(oDataset._properties);
     return super.update(oDataset).then((oData) => {
       return this.schemaRestDAO.getByNameAndVersion(oData.schemaName, oData.schemaVersion).then((oData) => {
         this.modelBinder.setProperty(oData, "/schema");
@@ -350,7 +351,7 @@ class DatasetService extends EntityService {
   }
 
   create(oDataset) {
-    this.replaceBlankProperties(oDataset);
+    oDataset._properties = this.stripBlankProperties(oDataset._properties);
     return super.create(oDataset);
   }
 
