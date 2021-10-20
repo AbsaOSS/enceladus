f520fd9da0ccf985ddc3431f715e588fcda17019
Adrian Olosutean <adi.olosutean@gmail.com>
2021-10-20 17:03:46 +0200

    1898 removed unused code

--- data-model/src/main/scala/za/co/absa/enceladus/model/ConformedSchema.scala
+++ data-model/src/main/scala/za/co/absa/enceladus/model/ConformedSchema.scala
@@ -15,9 +15,7 @@
 
 package za.co.absa.enceladus.model
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.expr
-import org.apache.spark.sql.types.{DataType, StringType, StructField, StructType}
+import org.apache.spark.sql.types.StructField
 import za.co.absa.enceladus.model.conformanceRule._
 
 case class ConformedSchema(schema: List[StructField], dataset: Dataset) {
@@ -42,61 +40,6 @@ case class ConformedSchema(schema: List[StructField], dataset: Dataset) {
           maybeRule.isEmpty
         }
     }
-  }
-}
-
-object ConformedSchema {
-
-  def create(dataset: Dataset, structFields: Array[StructField],
-            mappingTableSchemas: Map[String, Array[StructField]])(implicit spark: SparkSession): ConformedSchema = {
-    import spark.implicits._
-
-    val fields: List[StructField] = dataset.conformance.foldLeft(structFields.toList)((acc: List[StructField],
-                                                                                       conformanceRule: ConformanceRule) => {
-      conformanceRule match {
-        case UppercaseConformanceRule(_, outputColumn, _, inputColumn) => {
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumn, outputColumn, structFields))
-        }
-        case LiteralConformanceRule(_, outputColumn, _, value) => {
-          val fieldType = List(1).toDF().withColumn("dummy", expr(value)).schema.fields(1).dataType
-          acc ++ List(StructField(outputColumn, fieldType))
-        }
-        case NegationConformanceRule(_, outputColumn, _, inputColumn) =>
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumn, outputColumn, structFields))
-        case CastingConformanceRule(_, outputColumn, _, inputColumn, outputDataType: String) =>
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumn, outputColumn, structFields)
-            .copy(dataType = DataType.fromDDL(outputDataType)))
-        case MappingConformanceRule(_, _, mappingTable, _, _, targetAttribute, outputColumn, additionalColumns, _, _, _) =>
-          val mappingTableSchemaFields: Array[StructField] = mappingTableSchemas(mappingTable)
-          val fieldsMapping = additionalColumns.getOrElse(Map()) ++ Map(outputColumn -> targetAttribute)
-          val mappingFeilds = fieldsMapping.map{case (outputCol, targetAttribute) =>
-            getProjectedSchemaFieldFromStructField(targetAttribute, outputCol, mappingTableSchemaFields)
-          }.toList
-          acc ++ mappingFeilds
-        case SingleColumnConformanceRule(_, _, outputColumn, inputColumn, inputColumnAlias) =>
-          StructField(outputColumn, StructType(Seq(StructField(inputColumnAlias, StringType))))
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumn, outputColumn, structFields))
-        case DropConformanceRule(_, _, outputColumn) => acc.filterNot(_.name == outputColumn)
-        case ConcatenationConformanceRule(_, outputColumn, _, inputColumns) =>
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumns.head, outputColumn, structFields))
-        case CoalesceConformanceRule(_, outputColumn, _, inputColumns) =>
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumns.head, outputColumn, structFields))
-        case FillNullsConformanceRule(_, outputColumn, _, inputColumn, value) =>
-          acc ++ List(getProjectedSchemaFieldFromStructField(inputColumn, outputColumn, structFields))
-        case SparkSessionConfConformanceRule(_, outputColumn, _, sparkConfKey) => {
-          spark.sparkContext.getConf.get(sparkConfKey)
-          acc ++ List(StructField(name= outputColumn, StringType))
-        }
-        case _ => acc
-      }
-
-    })
-    ConformedSchema(fields, dataset)
-  }
-
-  def getProjectedSchemaFieldFromStructField(inputCol: String, outputCol: String, structFields: Array[StructField]): StructField = {
-    val value = structFields.find(_.name == inputCol).get
-    value.copy(name = outputCol)
   }
 }
 
