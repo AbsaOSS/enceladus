b18f57bcc65d9dc2d4035f2dc35860deabffde22
Daniel Kavan <dk1844@gmail.com>
2022-06-07 09:19:19 +0200

    #2075 pr fixes: merge fix

--- README.md
+++ README.md
@@ -356,9 +356,6 @@ A module containing [examples](examples/README.md) of the project usage.
 ## How to contribute
 Please see our [**Contribution Guidelines**](CONTRIBUTING.md).
 
-## Documentation
-Please see the [documentation pages](https://absaoss.github.io/enceladus/).
-
 # Extras
  - For Menas migration, there is a useful script available in
 [scripts/migration/migrate_menas.py](scripts/migration/migrate_menas.py)
--- spark-jobs/src/main/scala/za/co/absa/enceladus/common/CommonJobExecution.scala
+++ spark-jobs/src/main/scala/za/co/absa/enceladus/common/CommonJobExecution.scala
@@ -21,7 +21,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.FileSystem
 import org.apache.spark.sql.functions.{lit, to_date}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.apache.spark.{SPARK_VERSION, SparkConf}
+import org.apache.spark.SPARK_VERSION
 import org.slf4j.{Logger, LoggerFactory}
 import za.co.absa.atum.AtumImplicits._
 import za.co.absa.atum.core.{Atum, ControlType}
