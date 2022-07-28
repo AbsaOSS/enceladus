ba7b756435e96682f82dabf1fc92d3f9693951cf
David Benedeki <benedeki@volny.cz>
2022-07-28 09:55:58 +0200

    * Removed forgotten comment

--- utils/src/main/scala/za/co/absa/enceladus/utils/testUtils/TZNormalizedSparkTestBase.scala
+++ utils/src/main/scala/za/co/absa/enceladus/utils/testUtils/TZNormalizedSparkTestBase.scala
@@ -23,7 +23,6 @@ trait TZNormalizedSparkTestBase extends SparkTestBase {
   override protected def initSpark(implicit sparkConfig: SparkTestConfig): SparkSession = {
     val result = super.initSpark
 
-    //TODO make conditional on empty SparkTestBase.timezone, once SparkCommons 0.3.0 will have been released
     if (sparkConfig.timezone.isEmpty) {
       TimeZoneNormalizer.normalizeAll(result)
     }
