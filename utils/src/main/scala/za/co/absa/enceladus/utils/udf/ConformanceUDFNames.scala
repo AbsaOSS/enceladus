2cbb3ae5912b632a0f966490ad03d11c635070fc
David Benedeki <14905969+benedeki@users.noreply.github.com>
2023-02-20 00:03:58 +0100

    #2156: Small code improvements (#2158)
    * improving test warning filtering
    * changed validation to use `List` instead of `Array` to work with Spark Commons out of box
    * unified Java source and target settings.
    * removed unused stuff
    * `UDFLibrary` rename to `EnceladusUDFLibrary` to avoid confusion with Spark Standardization one
    * removed duplicate UDFs
    * removed unused UDF names
    * changed eventually to in `StandardizationExecutionSuite` to include only the file reading
    ---------
    Co-authored-by: AB024LL <miroslav.pojer@absa.africa>
    Co-authored-by: Daniel K <dk1844@gmail.com>

--- utils/src/main/scala/za/co/absa/enceladus/utils/udf/ConformanceUDFNames.scala
+++ utils/src/main/scala/za/co/absa/enceladus/utils/udf/ConformanceUDFNames.scala
@@ -17,23 +17,16 @@ package za.co.absa.enceladus.utils.udf
 
 import java.util.concurrent.atomic.AtomicInteger
 
-object UDFNames {
+object ConformanceUDFNames {
   private val counter = new AtomicInteger(0)
 
-  final val stdCastErr = "stdCastErr"
-  final val stdNullErr = "stdNullErr"
-  final val stdSchemaErr = "stdSchemaErr"
+  final val arrayDistinctErrors = "arrayDistinctErrors"
+  final val errorColumnAppend = "errorColumnAppend"
 
   final val confMappingErr = "confMappingErr"
   final val confCastErr = "confCastErr"
   final val confNegErr = "confNegErr"
   final val confLitErr = "confLitErr"
-
-  final val arrayDistinctErrors = "arrayDistinctErrors"
-  final val cleanErrCol = "cleanErrCol"
-  final val errorColumnAppend = "errorColumnAppend"
-
-  final val binaryUnbase64 = "binaryUnbase64"
 
   def uniqueUDFName(prefix: String, colName: String): String = {
     prefix + "_" + colName.replace('.', '_') + "_" + counter.getAndIncrement()
