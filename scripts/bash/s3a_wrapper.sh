256c07d983cced111c46cb43fc535917f64f1aed
Saša Zejnilović <zejnils@gmail.com>
2023-11-14 22:56:09 +0100

    2195 Add s3a wrapper around run scripts

--- scripts/bash/s3a_wrapper.sh
+++ scripts/bash/s3a_wrapper.sh
@@ -12,163 +12,113 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-# We have 2 states.
-#   Write Standardisation
-#   Write Conformance
-# The script will have to figure out which is being run and if it is being written to S3 or HDFS.
-#   In case of standardisation parsing the java properties supplied to figure out the standardisation path.
-#   In case of conformance calling Menas for info over REST API using supplied metadata
-# Then cleaning up the paths to conform to the path schema in query provided.
-
-# TODO - Auto load jceks files
+#!/bin/bash
 
 set -e
 
-# Source env variables
+# Source environment variables
 source "$(dirname "$0")/enceladus_env.sh"
-
-# Initialize variables
-keytab=""
-dataset_name=""
-dataset_version=""
-report_date=""
-report_version=""
 
 # The first argument is the name of the original script
 original_script="$(dirname "$0")/$(basename "$1")"
-
 # Shift the first argument so we can process the rest
 shift
 
-# Check if the original script exists
-if [[ ! -f "$original_script" ]]; then
-  echo "Error: The script '$original_script' does not exist in the current directory."
+hdfsPublishPath=""
+hdfsPath=""
+jceks_flag=""
+
+# Function to print error message and exit
+function error_exit() {
+  echo "Error: $1" >&2
   exit 1
-fi
+}
 
-# Initialize an array to hold the other arguments
-other_args=()
+# Function to get dataset information
+function get_dataset_info() {
+  local response=""
 
-# Loop through arguments
+  response=$(curl --negotiate -s -u : "$MENAS_API/dataset/$dataset_name/$dataset_version")
+  [[ $? -ne 0 ]] && error_exit "Could not load dataset info - $dataset_name v $dataset_version from Menas at $MENAS_API"
+
+  hdfsPublishPath=$(echo "$response" | jq -r '.hdfsPublishPath')
+  hdfsPath=$(echo "$response" | jq -r '.hdfsPath')
+  [[ $hdfsPublishPath == "null" || $hdfsPath == "null" ]] && error_exit "Could not find the required paths in the response."
+  return 0
+}
+
+# Function to handle JCEKS and set jceks_flag if need be
+function handle_jceks_path() {
+  if [[ $hdfsPublishPath =~ ^s3a://.* ]]; then
+    echo "hdfsPublishPath starts with s3a://. Using JCEKS file."
+    if [[ -z $jceks_path ]]; then
+      readwrite_jceks=$(curl -s -X GET -d "{\"ecs_path\":\"$hdfsPublishPath\"}" "$ECS_API_BUCKET" | jq -r '.readwrite_jceks')
+      [[ -z $readwrite_jceks ]] && error_exit "Could not find readwrite_jceks in the response."
+      bucket_name=$(echo "$hdfsPublishPath" | cut -d'/' -f3)
+      jceks_flag="--jceks-path \"spark.hadoop.fs.s3a.bucket.$bucket_name.security.credential.provider.path=jceks:$readwrite_jceks\""
+    else
+      echo "--jceks-path argument is set by user"
+      jceks_flag="--jceks-path $jceks_path"
+    fi
+  fi
+  return 0
+}
+
+# Function to clean up versions
+function cleanup_versions() {
+  local path=$1
+  local api=$2
+  echo "Cleaning versions for $path"
+  curl -s -X GET --header "x-api-key: $ECS_API_KEY" -d "{\"ecs_path\":\"${path#s3a://}\"}" "$api"
+  echo
+  curl -s -X DELETE --header "x-api-key: $ECS_API_KEY" -d "{\"ecs_path\":\"${path#s3a://}\"}" "$api"
+  echo
+  echo "Versions cleaned"
+  return 0
+}
+
+# Parse command line arguments
 while [[ $# -gt 0 ]]; do
   case "$1" in
-    --menas-auth-keytab)
-      keytab="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --dataset-name)
-      dataset_name="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --dataset-version)
-      dataset_version="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --report-date)
-      report_date="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    --report-version)
-      report_version="$2"
-      shift # past argument
-      shift # past value
-      ;;
-    *)    # unknown option
-      other_args+=("$1") # save it in an array for later
-      shift # past argument
-      ;;
+    --menas-auth-keytab) keytab="$2"; shift 2 ;;
+    --dataset-name) dataset_name="$2"; shift 2 ;;
+    --dataset-version) dataset_version="$2"; shift 2 ;;
+    --report-date) report_date="$2"; shift 2 ;;
+    --report-version) report_version="$2"; shift 2 ;;
+    --jceks-path) jceks_path="$2"; shift 2 ;;
+    *)
+      if [ -z "$1" ]; then
+        # If the argument is an empty string, add two quotes to represent it
+        other_args+=('""')
+      elif [ "$1" == "''" ]; then
+        # If the argument is ''
+        other_args+=("\'\'")
+      else
+        other_args+=("$1")
+      fi
+      shift ;;
   esac
 done
 
-# Print the extracted variables
-echo "Keytab: $keytab"
-echo "Dataset Name: $dataset_name"
-echo "Dataset Version: $dataset_version"
-echo "Report Date: $report_date"
-echo "Report Version: $report_version"
+[[ ! -f "$original_script" ]] && error_exit "The script '$original_script' does not exist in the current directory."
 
-# Run klist to check for a current Kerberos ticket
-if klist -s; then
-    echo "Kerberos ticket found."
-else
-    echo "No Kerberos ticket found or ticket is expired. Please run kinit."
-fi
+# Main script execution
+get_dataset_info
+handle_jceks_path
 
-# Get Dataset info
-response=$(curl --negotiate -s -u : "$MENAS_API/dataset/$dataset_name/$dataset_version")
-if [ $? -ne 0 ]; then
-    echo "Could not load dataset info - $dataset_name v $dataset_version from Menas at $MENAS_API"
-    exit 1
-fi
-
-# Parse the response using jq to extract hdfsPublishPath and hdfsPath
-hdfsPublishPath=$(echo "$response" | jq -r '.hdfsPublishPath')
-hdfsPath=$(echo "$response" | jq -r '.hdfsPath')
-
-# Check if the paths are null or not
-if [[ $hdfsPublishPath != "null" && $hdfsPath != "null" ]]; then
-    echo "hdfsPublishPath: $hdfsPublishPath"
-    echo "hdfsPath: $hdfsPath"
-else
-    echo "Could not find the required paths in the response."
-    exit 1
-fi
-
-# Run the original script with all the arguments
-"$original_script" "${other_args[@]}" \
+# Run the original script
+echo "$original_script" "${other_args[@]}" \
   --menas-auth-keytab "$keytab" \
   --dataset-name "$dataset_name" \
   --dataset-version "$dataset_version" \
   --report-date "$report_date" \
-  --report-version "$report_version"
+  --report-version "$report_version" \
+  "$jceks_flag" | bash
 
-# Save the exit code
 exit_code=$?
 
-# Run versions cleanup for publish on s3a
-if [[ $hdfsPublishPath == s3a://* ]]; then
-  echo "We have publish versions to clean:"
-  curl -X GET \
-    --header "x-api-key: $ECS_API_KEY" \
-    -d "{\"ecs_path\":\"${hdfsPublishPath#s3a://}\"}" \
-    $ECS_API_KK
-  echo
-  curl -X DELETE \
-    --header "x-api-key: $ECS_API_KEY" \
-    -d "{\"ecs_path\":\"${hdfsPublishPath#s3a://}\"}" \
-    $ECS_API_KK
-  echo
-  echo "Versions cleaned"
-else
-  echo "No publish versions to clean."
-fi
+# Clean up versions if necessary
+[[ $hdfsPublishPath == s3a://* ]] && cleanup_versions "$hdfsPublishPath" "$ECS_API_KK"
+[[ $STD_HDFS_PATH == s3a://* ]] && cleanup_versions "$STD_HDFS_PATH" "$ECS_API_KK"
 
-if [[ $STD_HDFS_PATH == s3a://* ]]; then
-  STD_HDFS_PATH_FILLED="${STD_HDFS_PATH//\{0\}/$dataset_name}"
-  STD_HDFS_PATH_FILLED="${STD_HDFS_PATH_FILLED//\{1\}/$dataset_version}"
-  STD_HDFS_PATH_FILLED="${STD_HDFS_PATH_FILLED//\{2\}/$report_date}"
-  STD_HDFS_PATH_FILLED="${STD_HDFS_PATH_FILLED//\{3\}/$report_version}"
-
-  echo "We have tmp versions to clean:"
-  curl -X GET \
-    --header "x-api-key: $ECS_API_KEY" \
-    -d "{\"ecs_path\":\"${STD_HDFS_PATH_FILLED#s3a://}\"}" \
-    $ECS_API_KK
-  echo
-  curl -X DELETE \
-    --header "x-api-key: $ECS_API_KEY" \
-    -d "{\"ecs_path\":\"${STD_HDFS_PATH_FILLED#s3a://}\"}" \
-    $ECS_API_KK
-  echo
-  echo "Versions cleaned"
-else
-  echo "No std versions to clean."
-fi
-
-# At the end of the script, use the saved exit code
 exit $exit_code
